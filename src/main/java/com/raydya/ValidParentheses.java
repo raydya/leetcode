5752d85eb8a026653dddbd9a68c80597b35ca7d5
zhanglei <dya.ilaempire@gmail.com>
2020-09-24 11:53:46 +0800

    premitive

--- src/main/java/com/raydya/ValidParentheses.java
+++ src/main/java/com/raydya/ValidParentheses.java
@@ -2,15 +2,10 @@ package com.raydya;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.HashMap;
 
 public class ValidParentheses {
 
     public boolean isValid(String s) {
-        return primitiveImpl(s);
-    }
-
-    private boolean primitiveImpl(String s) {
         final char[] chars = s.toCharArray();
 
         Deque<Character> stackLeft = new ArrayDeque<>();
@@ -33,47 +28,6 @@ public class ValidParentheses {
                     stackLeft.pop();
                 } else {
                     stackRight.push(c);
-                }
-            }
-        }
-
-        return stackLeft.isEmpty() && stackRight.isEmpty();
-    }
-
-    private boolean stackImpl(String s) {
-        final HashMap<String, Boolean> leftMap = new HashMap<>();
-        leftMap.put("(", true);
-        leftMap.put("[", true);
-        leftMap.put("{", true);
-        leftMap.put(")", false);
-        leftMap.put("]", false);
-        leftMap.put("}", false);
-
-        final HashMap<String, String> matchMap = new HashMap<>();
-        matchMap.put("(", ")");
-        matchMap.put("[", "]");
-        matchMap.put("{", "}");
-
-        Deque<String> stackLeft = new ArrayDeque<>();
-        Deque<String> stackRight = new ArrayDeque<>();
-
-        final String[] strings = s.split("");
-        for (final String string : strings) {
-            final Boolean isLeft = leftMap.get(string);
-            if (Boolean.TRUE.equals(isLeft)) {
-                stackLeft.push(string);
-            } else {
-                if (stackLeft.isEmpty()) {
-                    stackRight.push(string);
-                    continue;
-                }
-
-                final String leftFirst = stackLeft.getFirst();
-                final String rightParentheses = matchMap.get(leftFirst);
-                if (rightParentheses.equals(string)) {
-                    stackLeft.pop();
-                } else {
-                    stackRight.push(string);
                 }
             }
         }
