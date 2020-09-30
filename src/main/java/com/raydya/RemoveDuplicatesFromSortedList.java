2b3f12049d7ab659e1e905eb520a8867d4a1f037
zhanglei <dya.ilaempire@gmail.com>
2020-09-30 10:48:29 +0800

    remove duplicates from sorted list

---
+++ src/main/java/com/raydya/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,24 @@
+package com.raydya;
+
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+
+        ListNode tmp = head;
+        while (tmp.next != null) {
+            final ListNode next = tmp.next;
+            final int val = tmp.val;
+            final int nextVal = next.val;
+
+            if (val == nextVal) {
+                tmp.next = next.next;
+                next.next = null;
+            } else {
+                tmp = next;
+            }
+        }
+
+        return head;
+    }
+}
---
+++ src/test/java/com/raydya/RemoveDuplicatesFromSortedListTest.java
@@ -0,0 +1,55 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveDuplicatesFromSortedListTest {
+    @Test
+    public void test1() {
+        final ListNode a3 = new ListNode(2);
+        final ListNode a2 = new ListNode(1, a3);
+        final ListNode a1 = new ListNode(1, a2);
+
+        final RemoveDuplicatesFromSortedList solution = new RemoveDuplicatesFromSortedList();
+        final ListNode head = solution.deleteDuplicates(a1);
+
+        Assert.assertEquals(1, head.val);
+        Assert.assertEquals(2, head.next.val);
+    }
+
+    @Test
+    public void test2() {
+        final ListNode a5 = new ListNode(3);
+        final ListNode a4 = new ListNode(3, a5);
+        final ListNode a3 = new ListNode(2, a4);
+        final ListNode a2 = new ListNode(1, a3);
+        final ListNode a1 = new ListNode(1, a2);
+
+        final RemoveDuplicatesFromSortedList solution = new RemoveDuplicatesFromSortedList();
+        final ListNode head = solution.deleteDuplicates(a1);
+
+        Assert.assertEquals(1, head.val);
+        Assert.assertEquals(2, head.next.val);
+        Assert.assertEquals(3, head.next.next.val);
+    }
+
+    @Test
+    public void test3() {
+        final ListNode a3 = new ListNode(1);
+        final ListNode a2 = new ListNode(1, a3);
+        final ListNode a1 = new ListNode(1, a2);
+
+        final RemoveDuplicatesFromSortedList solution = new RemoveDuplicatesFromSortedList();
+        final ListNode head = solution.deleteDuplicates(a1);
+
+        Assert.assertEquals(1, head.val);
+    }
+
+    @Test
+    public void test4() {
+        final RemoveDuplicatesFromSortedList solution = new RemoveDuplicatesFromSortedList();
+        final ListNode head = solution.deleteDuplicates(null);
+
+        Assert.assertNull(head);
+    }
+}
