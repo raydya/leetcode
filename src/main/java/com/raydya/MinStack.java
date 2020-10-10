a67a7ee2c85251c9159cd562f9244c305cbcb8ac
zhanglei <dya.ilaempire@gmail.com>
2020-10-10 18:24:11 +0800

    min stack

---
+++ src/main/java/com/raydya/MinStack.java
@@ -0,0 +1,41 @@
+package com.raydya;
+
+import com.raydya.data.type.ListNode;
+
+public class MinStack {
+    private ListNode top;
+    private int min = Integer.MAX_VALUE;
+
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        final ListNode n = new ListNode(x);
+        n.next = top;
+        top = n;
+        min = Math.min(min, x);
+    }
+
+    public void pop() {
+        final int popVal = top.val;
+        top = top.next;
+
+        if (popVal == min) {
+            ListNode tmp = top;
+            min = Integer.MAX_VALUE;
+            while (tmp != null) {
+                min = Math.min(min, tmp.val);
+                tmp = tmp.next;
+            }
+        }
+    }
+
+    public int top() {
+        return top.val;
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
---
+++ src/test/java/com/raydya/MinStackTest.java
@@ -0,0 +1,20 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinStackTest {
+    @Test
+    public void test1() {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        Assert.assertEquals(-3, minStack.getMin());
+
+        minStack.pop();
+        Assert.assertEquals(0, minStack.top());
+
+        Assert.assertEquals(-2, minStack.getMin());
+    }
+}
