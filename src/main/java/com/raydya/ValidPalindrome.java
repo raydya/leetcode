1a6b9f8c29291b315f814011c3a8d700da8f26f2
zhanglei <dya.ilaempire@gmail.com>
2020-10-10 10:55:06 +0800

    valid palindrome

---
+++ src/main/java/com/raydya/ValidPalindrome.java
@@ -0,0 +1,37 @@
+package com.raydya;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        final char[] chars = s.toCharArray();
+        int i = 0;
+        int j = chars.length - 1;
+
+        while (j >= i) {
+            final char a = chars[i];
+            final char b = chars[j];
+
+            if (!Character.isLetterOrDigit(a)) {
+                i++;
+                continue;
+            }
+
+            if (!Character.isLetterOrDigit(b)) {
+                j--;
+                continue;
+            }
+
+            if (Character.toLowerCase(a) != Character.toLowerCase(b)) {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/raydya/ValidPalindromeTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidPalindromeTest {
+    @Test
+    public void test1() {
+        final ValidPalindrome solution = new ValidPalindrome();
+        final boolean isPalindrome = solution.isPalindrome("A man, a plan, a canal: Panama");
+
+        Assert.assertTrue(isPalindrome);
+    }
+
+    @Test
+    public void test2() {
+        final ValidPalindrome solution = new ValidPalindrome();
+        final boolean isPalindrome = solution.isPalindrome("race a car");
+
+        Assert.assertFalse(isPalindrome);
+    }
+}
