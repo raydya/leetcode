797a7865e41c486dc57e8f4e03917ac52e978833
zhanglei <dya.ilaempire@gmail.com>
2020-10-20 10:55:09 +0800

    feat: count primes

---
+++ src/main/java/com/raydya/CountPrimes.java
@@ -0,0 +1,35 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class CountPrimes {
+    /*
+     * Sieve_of_Eratosthenes
+     * 埃拉托斯特尼筛法
+     * https://zh.wikipedia.org/wiki/%E5%9F%83%E6%8B%89%E6%89%98%E6%96%AF%E7%89%B9%E5%B0%BC%E7%AD%9B%E6%B3%95
+     */
+    public int countPrimes(int n) {
+        if (n <= 1) return 0;
+
+        final boolean[] table = new boolean[n];
+        Arrays.fill(table, true);
+
+        int c = 0;
+
+        final double sqrt = Math.sqrt(n);
+
+        for (int i = 2; i < table.length; i++) {
+            if (table[i]) {
+                c++;
+
+                if (i <= sqrt) {
+                    for (int j = i * i; j < n; j += i) {
+                        table[j] = false;
+                    }
+                }
+            }
+        }
+
+        return c;
+    }
+}
---
+++ src/test/java/com/raydya/CountPrimesTest.java
@@ -0,0 +1,54 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CountPrimesTest {
+    @Test
+    public void test1() {
+        final CountPrimes solution = new CountPrimes();
+        final int countPrimes = solution.countPrimes(10);
+
+        Assert.assertEquals(4, countPrimes);
+    }
+
+    @Test
+    public void test2() {
+        final CountPrimes solution = new CountPrimes();
+        final int countPrimes = solution.countPrimes(0);
+
+        Assert.assertEquals(0, countPrimes);
+    }
+
+    @Test
+    public void test3() {
+        final CountPrimes solution = new CountPrimes();
+        final int countPrimes = solution.countPrimes(1);
+
+        Assert.assertEquals(0, countPrimes);
+    }
+
+    @Test
+    public void test4() {
+        final CountPrimes solution = new CountPrimes();
+        final int countPrimes = solution.countPrimes(120);
+
+        Assert.assertEquals(30, countPrimes);
+    }
+
+    @Test
+    public void test5() {
+        final CountPrimes solution = new CountPrimes();
+        final int countPrimes = solution.countPrimes(2);
+
+        Assert.assertEquals(0, countPrimes);
+    }
+
+    @Test
+    public void test6() {
+        final CountPrimes solution = new CountPrimes();
+        final int countPrimes = solution.countPrimes(10000);
+
+        Assert.assertEquals(1229, countPrimes);
+    }
+}
