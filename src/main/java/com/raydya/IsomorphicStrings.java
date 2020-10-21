b0d420d59182c5a9d8a75597945cf8f2f99dd887
zhanglei <dya.ilaempire@gmail.com>
2020-10-21 18:15:18 +0800

    isomorphic strings

--- src/main/java/com/raydya/IsomorphicStrings.java
+++ src/main/java/com/raydya/IsomorphicStrings.java
@@ -1,2 +1,38 @@
-package com.raydya;public class IsomorphicStrings {
+package com.raydya;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Character> map = new HashMap<>();
+
+        final char[] as = new char[s.length()];
+        final char[] at = new char[t.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            as[i] = s.charAt(i);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            at[i] = t.charAt(i);
+        }
+
+        for (int i = 0; i < as.length; i++) {
+            final char ss = as[i];
+            final char st = at[i];
+
+            if (!map.containsKey(ss) && !map.containsValue(st)) {
+                map.put(ss, st);
+            }
+
+            if (!map.containsKey(ss) && map.containsValue(st) || !map.get(ss).equals(st)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
--- src/test/java/com/raydya/IsomorphicStringsTest.java
+++ src/test/java/com/raydya/IsomorphicStringsTest.java
@@ -1,2 +1,38 @@
-package com.raydya;public class IsomorphicStringsTest {
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IsomorphicStringsTest {
+    @Test
+    public void test1() {
+        final IsomorphicStrings solution = new IsomorphicStrings();
+        final boolean isomorphic = solution.isIsomorphic("egg", "add");
+
+        Assert.assertTrue(isomorphic);
+    }
+
+    @Test
+    public void test2() {
+        final IsomorphicStrings solution = new IsomorphicStrings();
+        final boolean isomorphic = solution.isIsomorphic("foo", "bar");
+
+        Assert.assertFalse(isomorphic);
+    }
+
+    @Test
+    public void test3() {
+        final IsomorphicStrings solution = new IsomorphicStrings();
+        final boolean isomorphic = solution.isIsomorphic("paper", "title");
+
+        Assert.assertTrue(isomorphic);
+    }
+
+    @Test
+    public void test4() {
+        final IsomorphicStrings solution = new IsomorphicStrings();
+        final boolean isomorphic = solution.isIsomorphic("ab", "aa");
+
+        Assert.assertFalse(isomorphic);
+    }
 }
