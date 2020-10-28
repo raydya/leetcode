c453718a1a002b7cd91c7e5e41e044aa7de3973e
zhanglei <dya.ilaempire@gmail.com>
2020-10-28 10:12:43 +0800

    valid anagram

---
+++ src/main/java/com/raydya/ValidAnagram.java
@@ -0,0 +1,42 @@
+package com.raydya;
+
+public class ValidAnagram {
+//    public boolean isAnagram(String s, String t) {
+//        if (s.length() != t.length()) return false;
+//
+//        final int[] h1 = new int[256];
+//        final int[] h2 = new int[256];
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            final char c = s.charAt(i);
+//            h1[c]++;
+//        }
+//
+//        for (int i = 0; i < t.length(); i++) {
+//            final char c = t.charAt(i);
+//            h2[c]++;
+//        }
+//
+//        return Arrays.equals(h1, h2);
+//    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        final int[] table = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            final int c = s.charAt(i) - 'a';
+            table[c]++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            final int c = t.charAt(i) - 'a';
+            table[c]--;
+
+            if (table[c] < 0) return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/raydya/ValidAnagramTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidAnagramTest {
+    @Test
+    public void test1() {
+        final ValidAnagram solution = new ValidAnagram();
+        final boolean anagram = solution.isAnagram("anagram", "nagaram");
+
+        Assert.assertTrue(anagram);
+    }
+
+    @Test
+    public void test2() {
+        final ValidAnagram solution = new ValidAnagram();
+        final boolean anagram = solution.isAnagram("rat", "car");
+
+        Assert.assertFalse(anagram);
+    }
+}
