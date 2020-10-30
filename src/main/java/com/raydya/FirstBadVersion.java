60833ee0cefb417d917a70e103fe818b15526af5
zhanglei <dya.ilaempire@gmail.com>
2020-10-30 14:40:54 +0800

    first bad version

---
+++ src/main/java/com/raydya/FirstBadVersion.java
@@ -0,0 +1,19 @@
+package com.raydya;
+
+import com.raydya.data.type.VersionControl;
+
+public class FirstBadVersion extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
