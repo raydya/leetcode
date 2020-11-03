06dbb46d02dbc30e2f2efa893392fe27df4329de
zhanglei <dya.ilaempire@gmail.com>
2020-11-03 10:54:53 +0800

    word pattern

---
+++ src/main/java/com/raydya/WordPattern.java
@@ -0,0 +1,31 @@
+package com.raydya;
+
+import java.util.HashMap;
+
+public class WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+        final String[] words = s.split(" ");
+
+        if (words.length != pattern.length()) return false;
+
+        final HashMap<String, String> holder = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            final char c = pattern.charAt(i);
+
+            final String word = words[i];
+
+            final String key = String.valueOf(c);
+
+            if (!holder.containsKey(key)) {
+                if (holder.containsValue(word)) return false;
+                holder.put(key, word);
+            } else {
+                final String h = holder.get(key);
+                if (!h.equals(word)) return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/raydya/WordPatternTest.java
@@ -0,0 +1,54 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WordPatternTest {
+    @Test
+    public void test1() {
+        final WordPattern solution = new WordPattern();
+        final boolean wordPattern = solution.wordPattern("abba", "dog cat cat dog");
+
+        Assert.assertTrue(wordPattern);
+    }
+
+    @Test
+    public void test2() {
+        final WordPattern solution = new WordPattern();
+        final boolean wordPattern = solution.wordPattern("abba", "dog cat cat fish");
+
+        Assert.assertFalse(wordPattern);
+    }
+
+    @Test
+    public void test3() {
+        final WordPattern solution = new WordPattern();
+        final boolean wordPattern = solution.wordPattern("aaaa", "dog cat cat dog");
+
+        Assert.assertFalse(wordPattern);
+    }
+
+    @Test
+    public void test4() {
+        final WordPattern solution = new WordPattern();
+        final boolean wordPattern = solution.wordPattern("abba", "dog dog dog dog");
+
+        Assert.assertFalse(wordPattern);
+    }
+
+    @Test
+    public void test5() {
+        final WordPattern solution = new WordPattern();
+        final boolean wordPattern = solution.wordPattern("abc", "b c a");
+
+        Assert.assertTrue(wordPattern);
+    }
+
+    @Test
+    public void test6() {
+        final WordPattern solution = new WordPattern();
+        final boolean wordPattern = solution.wordPattern("aaa", "aa aa aa aa");
+
+        Assert.assertFalse(wordPattern);
+    }
+}
