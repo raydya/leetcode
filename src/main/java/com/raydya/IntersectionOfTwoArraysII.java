69a85a7f688cccc066dd7fccfffc2e1f1f1ea41d
zhanglei <dya.ilaempire@gmail.com>
2020-11-05 16:03:53 +0800

    optimize intersection of two array ii

--- src/main/java/com/raydya/IntersectionOfTwoArraysII.java
+++ src/main/java/com/raydya/IntersectionOfTwoArraysII.java
@@ -4,35 +4,23 @@ import java.util.Arrays;
 
 public class IntersectionOfTwoArraysII {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int[] iterator = nums1.length > nums2.length ? nums2 : nums1;
-        int[] target = nums1.length > nums2.length ? nums1 : nums2;
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
 
-        int[] holder = new int[]{};
-
-        for (final int i : iterator) {
-            final int b = contains(holder, i);
-            if (b > 0) continue;
-
-            final int c = contains(target, i);
-            if (c > 0) {
-                final int a = contains(iterator, i);
-                final int d = Math.min(a, c);
-                final int[] ints = Arrays.copyOf(holder, holder.length + d);
-                for (int j = ints.length - 1; j > ints.length - 1 - d; j--) {
-                    ints[j] = i;
-                }
-                holder = ints;
+        int f = 0, s = 0;
+        int result = 0;
+        while (f < nums1.length && s < nums2.length) {
+            if (nums1[f] < nums2[s]) {
+                f++;
+            } else if (nums1[f] > nums2[s]) {
+                s++;
+            } else {
+                nums1[result++] = nums1[f];
+                f++;
+                s++;
             }
         }
 
-        return holder;
-    }
-
-    private int contains(int[] ints, int num) {
-        int c = 0;
-        for (final int anInt : ints) {
-            if (anInt == num) c++;
-        }
-        return c;
+        return Arrays.copyOf(nums1, result);
     }
 }
