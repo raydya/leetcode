905aae707ac78d60d03a11ebf0c190798e47c334
zhanglei <dya.ilaempire@gmail.com>
2020-11-09 13:46:46 +0800

    sum of left leaves

---
+++ src/main/java/com/raydya/SumOfLeftLeaves.java
@@ -0,0 +1,45 @@
+package com.raydya;
+
+import com.raydya.data.type.TreeNode;
+
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        return sum(root, 0);
+    }
+
+    public int sum(TreeNode node, int sum) {
+        if (node == null) return sum;
+        final TreeNode left = node.left;
+        final TreeNode right = node.right;
+        if (left != null && left.left == null && left.right == null) {
+            sum += left.val;
+        }
+        final int sum1 = sum(left, 0);
+        final int sum2 = sum(right, 0);
+
+        return sum + sum1 + sum2;
+    }
+
+//    public int sumOfLeftLeaves(TreeNode root) {
+//        final List<TreeNode> nodes = getLeftLeaves(root, new ArrayList<>());
+//
+//        return nodes.stream().map(node -> node.val)
+//            .reduce(Integer::sum)
+//            .orElse(0);
+//    }
+//
+//    private List<TreeNode> getLeftLeaves(TreeNode node, List<TreeNode> leaves) {
+//        if (node == null) return leaves;
+//        final TreeNode left = node.left;
+//        final TreeNode right = node.right;
+//        if (left != null && left.left == null && left.right == null) {
+//            leaves.add(left);
+//        }
+//        final List<TreeNode> leftLeaves = getLeftLeaves(left, new ArrayList<>());
+//        final List<TreeNode> rightLeaves = getLeftLeaves(right, new ArrayList<>());
+//        leaves.addAll(leftLeaves);
+//        leaves.addAll(rightLeaves);
+//
+//        return leaves;
+//    }
+}
---
+++ src/test/java/com/raydya/SumOfLeftLeavesTest.java
@@ -0,0 +1,44 @@
+package com.raydya;
+
+import com.raydya.data.type.TreeNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SumOfLeftLeavesTest {
+    @Test
+    public void test1() {
+        final TreeNode n4 = new TreeNode(7);
+        final TreeNode n3 = new TreeNode(15);
+
+        final TreeNode n2 = new TreeNode(20, n3, n4);
+        final TreeNode n1 = new TreeNode(9);
+        final TreeNode root = new TreeNode(3, n1, n2);
+
+        final SumOfLeftLeaves solution = new SumOfLeftLeaves();
+        final int sumOfLeftLeaves = solution.sumOfLeftLeaves(root);
+
+        Assert.assertEquals(24, sumOfLeftLeaves);
+    }
+
+    @Test
+    public void test2() {
+        final TreeNode n8 = new TreeNode(8);
+
+        final TreeNode n7 = new TreeNode(1);
+        final TreeNode n6 = new TreeNode(5);
+
+        final TreeNode n5 = new TreeNode(-1, null, n8);
+        final TreeNode n4 = new TreeNode(3, null, n7);
+
+        final TreeNode n3 = new TreeNode(1, n6, n7);
+
+        final TreeNode n2 = new TreeNode(4, n4, n5);
+        final TreeNode n1 = new TreeNode(2, n3, null);
+        final TreeNode root = new TreeNode(0, n1, n2);
+
+        final SumOfLeftLeaves solution = new SumOfLeftLeaves();
+        final int sumOfLeftLeaves = solution.sumOfLeftLeaves(root);
+
+        Assert.assertEquals(5, sumOfLeftLeaves);
+    }
+}
