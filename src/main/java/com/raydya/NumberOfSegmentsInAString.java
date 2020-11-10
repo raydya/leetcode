f2be56c005c453f78b9e2220d42b9760d00ca7e1
zhanglei <dya.ilaempire@gmail.com>
2020-11-10 18:29:08 +0800

    optmized number of segments in a string

--- src/main/java/com/raydya/NumberOfSegmentsInAString.java
+++ src/main/java/com/raydya/NumberOfSegmentsInAString.java
@@ -1,14 +1,13 @@
 package com.raydya;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class NumberOfSegmentsInAString {
     public int countSegments(String s) {
-        final List<String> strings = Arrays.stream(s.split(" "))
-            .filter(s1 -> !"".equals(s1))
-            .collect(Collectors.toList());
-        return strings.size();
+        int segmentCount = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') segmentCount++;
+        }
+
+        return segmentCount;
     }
 }
