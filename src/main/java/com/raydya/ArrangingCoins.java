1a04297e6a25ad95e0b87a3f25c2e63794a36a7e
zhanglei <dya.ilaempire@gmail.com>
2020-11-11 14:21:53 +0800

    some comment

--- src/main/java/com/raydya/ArrangingCoins.java
+++ src/main/java/com/raydya/ArrangingCoins.java
@@ -1,6 +1,10 @@
 package com.raydya;
 
 public class ArrangingCoins {
+    /**
+     * we got O(log n) [binary search] & O(1) [math] solutions
+     * but i dont wanna implement it
+     */
     public int arrangeCoins(int n) {
         int l = 0;
 
