d31a2a2b533fa759b63a0f818b60434ead131d13
zhanglei <dya.ilaempire@gmail.com>
2020-11-16 17:07:51 +0800

    str to integer

---
+++ src/main/java/com/raydya/StringToInteger.java
@@ -0,0 +1,29 @@
+package com.raydya;
+
+public class StringToInteger {
+    public int myAtoi(String s) {
+        if (s.length() == 0) return 0;
+
+        int i = 0;
+        int sign = 1;
+        int result = 0;
+
+        //Discard whitespaces in the beginning
+        while (i < s.length() && s.charAt(i) == ' ')
+            i++;
+
+        // Check if optional sign if it exists
+        if (i < s.length() && (s.charAt(i) == '+' || s.charAt(i) == '-'))
+            sign = (s.charAt(i++) == '-') ? -1 : 1;
+
+        // Build the result and check for overflow/underflow condition
+        while (i < s.length() && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            if (result > Integer.MAX_VALUE / 10 ||
+                (result == Integer.MAX_VALUE / 10 && s.charAt(i) - '0' > Integer.MAX_VALUE % 10)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + (s.charAt(i++) - '0');
+        }
+        return result * sign;
+    }
+}
---
+++ src/test/java/com/raydya/StringToIntegerTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StringToIntegerTest {
+    @Test
+    public void test1() {
+        final StringToInteger solution = new StringToInteger();
+        final int atoi = solution.myAtoi("42");
+
+        Assert.assertEquals(42, atoi);
+    }
+
+    @Test
+    public void test2() {
+        final StringToInteger solution = new StringToInteger();
+        final int atoi = solution.myAtoi("   -42");
+
+        Assert.assertEquals(-42, atoi);
+    }
+
+    @Test
+    public void test3() {
+        final StringToInteger solution = new StringToInteger();
+        final int atoi = solution.myAtoi("4193 with words");
+
+        Assert.assertEquals(4193, atoi);
+    }
+
+    @Test
+    public void test4() {
+        final StringToInteger solution = new StringToInteger();
+        final int atoi = solution.myAtoi("words and 987");
+
+        Assert.assertEquals(0, atoi);
+    }
+
+    @Test
+    public void test5() {
+        final StringToInteger solution = new StringToInteger();
+        final int atoi = solution.myAtoi("-91283472332");
+
+        Assert.assertEquals(-2147483648, atoi);
+    }
+}
