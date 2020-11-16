c93f4eab735f8ab190b7cc23e5c102827b7db27c
zhanglei <dya.ilaempire@gmail.com>
2020-11-16 13:35:49 +0800

    find all numbers disappeared in an array

---
+++ src/main/java/com/raydya/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInAnArray {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
+            }
+        }
+        List<Integer> results = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                results.add(i + 1);
+            }
+        }
+        return results;
+    }
+}
---
+++ src/test/java/com/raydya/FindAllNumberDisappearedInAnArrayTest.java
@@ -0,0 +1,50 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FindAllNumberDisappearedInAnArrayTest {
+    @Test
+    public void test1() {
+        final FindAllNumbersDisappearedInAnArray solution = new FindAllNumbersDisappearedInAnArray();
+        final List<Integer> disappearedNumbers = solution.findDisappearedNumbers(new int[]{4, 3, 2, 7, 8, 2, 3, 1});
+
+        Assert.assertArrayEquals(Arrays.asList(5, 6).toArray(), disappearedNumbers.toArray());
+    }
+
+    @Test
+    public void test2() {
+        final FindAllNumbersDisappearedInAnArray solution = new FindAllNumbersDisappearedInAnArray();
+        final List<Integer> disappearedNumbers = solution.findDisappearedNumbers(new int[]{});
+
+        Assert.assertArrayEquals(Collections.emptyList().toArray(), disappearedNumbers.toArray());
+    }
+
+    @Test
+    public void test3() {
+        final FindAllNumbersDisappearedInAnArray solution = new FindAllNumbersDisappearedInAnArray();
+        final List<Integer> disappearedNumbers = solution.findDisappearedNumbers(new int[]{1, 1});
+
+        Assert.assertArrayEquals(Collections.singletonList(2).toArray(), disappearedNumbers.toArray());
+    }
+
+    @Test
+    public void test4() {
+        final FindAllNumbersDisappearedInAnArray solution = new FindAllNumbersDisappearedInAnArray();
+        final List<Integer> disappearedNumbers = solution.findDisappearedNumbers(new int[]{1, 1, 2, 2});
+
+        Assert.assertArrayEquals(Arrays.asList(3, 4).toArray(), disappearedNumbers.toArray());
+    }
+
+    @Test
+    public void test5() {
+        final FindAllNumbersDisappearedInAnArray solution = new FindAllNumbersDisappearedInAnArray();
+        final List<Integer> disappearedNumbers = solution.findDisappearedNumbers(new int[]{5, 4, 6, 7, 9, 3, 10, 9, 5, 6});
+
+        Assert.assertArrayEquals(Arrays.asList(1, 2, 8).toArray(), disappearedNumbers.toArray());
+    }
+}
