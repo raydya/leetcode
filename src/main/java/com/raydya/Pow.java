b06ac77ba5b64d239631939f1d681a2ce6017b47
zhanglei <dya.ilaempire@gmail.com>
2020-12-04 10:37:22 +0800

    my pow

---
+++ src/main/java/com/raydya/Pow.java
@@ -0,0 +1,24 @@
+package com.raydya;
+
+public class Pow {
+    public double myPow(double x, int n) {
+        double res = 1.0;
+
+        while (n != 0) {
+            if (Math.abs(n) % 2 == 0) {
+                x *= x;
+                n /= 2;
+            }
+
+            if (n > 0) {
+                res *= x;
+                n--;
+            } else {
+                res *= (1 / x);
+                n++;
+            }
+        }
+
+        return res;
+    }
+}
---
+++ src/test/java/com/raydya/PowTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PowTest {
+    @Test
+    public void test1() {
+        final Pow solution = new Pow();
+        final double res = solution.myPow(2.00000, 10);
+
+        Assert.assertEquals(1024.00000, res, 0);
+    }
+
+    @Test
+    public void test2() {
+        final Pow solution = new Pow();
+        final double res = solution.myPow(2.10000, 3);
+
+        Assert.assertEquals(9.26100, res, 1);
+    }
+
+    @Test
+    public void test3() {
+        final Pow solution = new Pow();
+        final double res = solution.myPow(2.00000, -2);
+
+        Assert.assertEquals(0.25000, res, 0);
+    }
+
+    @Test
+    public void test4() {
+        final Pow solution = new Pow();
+        final double res = solution.myPow(1.00000, 2147483647);
+
+        Assert.assertEquals(1.00000, res, 0);
+    }
+
+    @Test
+    public void test5() {
+        final Pow solution = new Pow();
+        final double res = solution.myPow(2.00000, -2147483648);
+
+        Assert.assertEquals(0.00000, res, 0);
+    }
+}
