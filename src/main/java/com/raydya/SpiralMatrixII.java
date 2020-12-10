0fc3560869e96889e90b6c2fd2331bfa698a0f2d
zhanglei <dya.ilaempire@gmail.com>
2020-12-10 09:52:12 +0800

    spiral matrix II

---
+++ src/main/java/com/raydya/SpiralMatrixII.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+public class SpiralMatrixII {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+
+        int r1 = 0, r2 = n - 1;
+        int c1 = 0, c2 = n - 1;
+        int num = 1;
+
+        // LAYER BY LAYER
+        while (r1 <= r2 && c1 <= c2) {
+            // TOP
+            for (int c = c1; c <= c2; c++) {
+                matrix[r1][c] = num;
+                num++;
+            }
+            // RIGHT
+            for (int r = r1 + 1; r <= r2; r++) {
+                matrix[r][c2] = num;
+                num++;
+            }
+
+            if (r1 < r2 && c1 < c2) {
+                // BOTTOM
+                for (int c = c2 - 1; c > c1; c--) {
+                    matrix[r2][c] = num;
+                    num++;
+                }
+
+                // LEFT
+                for (int r = r2; r > r1; r--) {
+                    matrix[r][c1] = num;
+                    num++;
+                }
+            }
+            // INNER LAYER
+            r1++;
+            r2--;
+            c1++;
+            c2--;
+        }
+
+        return matrix;
+    }
+}
---
+++ src/test/java/com/raydya/SpiralMatrixIITest.java
@@ -0,0 +1,27 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SpiralMatrixIITest {
+    @Test
+    public void test1() {
+        final SpiralMatrixII solution = new SpiralMatrixII();
+        final int[][] matrix = solution.generateMatrix(3);
+
+        Assert.assertArrayEquals(
+            new int[][]{
+                {1, 2, 3}, {8, 9, 4}, {7, 6, 5}
+            },
+            matrix
+        );
+    }
+
+    @Test
+    public void test2() {
+        final SpiralMatrixII solution = new SpiralMatrixII();
+        final int[][] matrix = solution.generateMatrix(1);
+
+        Assert.assertArrayEquals(new int[][]{{1}}, matrix);
+    }
+}
