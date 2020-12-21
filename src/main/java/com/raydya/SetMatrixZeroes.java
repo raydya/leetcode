165f5a366229f932d228476cc0c74ff58da20198
zhanglei <dya.ilaempire@gmail.com>
2020-12-21 11:34:18 +0800

    set matrix zero

---
+++ src/main/java/com/raydya/SetMatrixZeroes.java
@@ -0,0 +1,51 @@
+package com.raydya;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        Boolean isCol = false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 0; i < m; i++) {
+
+            // Since first cell for both first row and first column is the same i.e. matrix[0][0]
+            // We can use an additional variable for either the first row/column.
+            // For this solution we are using an additional variable for the first column
+            // and using matrix[0][0] for the first row.
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+
+            for (int j = 1; j < n; j++) {
+                // If an element is zero, we set the first element of the corresponding row and column to 0
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        // Iterate over the array once again and using the first row and first column, update the elements.
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // See if the first row needs to be set to zero as well
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // See if the first column needs to be set to zero as well
+        if (isCol) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/raydya/SetMatrixZeroesTest.java
@@ -0,0 +1,24 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SetMatrixZeroesTest {
+    @Test
+    public void test1() {
+        final int[][] input = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        final SetMatrixZeroes solution = new SetMatrixZeroes();
+        solution.setZeroes(input);
+
+        Assert.assertArrayEquals(new int[][]{{1, 0, 1}, {0, 0, 0}, {1, 0, 1}}, input);
+    }
+
+    @Test
+    public void test2() {
+        final int[][] input = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        final SetMatrixZeroes solution = new SetMatrixZeroes();
+        solution.setZeroes(input);
+
+        Assert.assertArrayEquals(new int[][]{{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}}, input);
+    }
+}
