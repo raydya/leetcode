c782e9b2a65ede34927b9c7f81994c08d3a35db4
zhanglei <dya.ilaempire@gmail.com>
2020-12-22 14:55:37 +0800

    sorted colors

---
+++ src/main/java/com/raydya/SortColors.java
@@ -0,0 +1,19 @@
+package com.raydya;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        final int[] holder = new int[3];
+        for (final int v : nums) {
+            holder[v]++;
+        }
+
+        int c = 0;
+        for (int i = 0; i < holder.length; i++) {
+            final int i1 = holder[i];
+            for (int j = 0; j < i1; j++) {
+                nums[c] = i;
+                c++;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/raydya/SortColorsTest.java
@@ -0,0 +1,42 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SortColorsTest {
+    @Test
+    public void test1() {
+        final int[] colors = {2, 0, 2, 1, 1, 0};
+        final SortColors solution = new SortColors();
+        solution.sortColors(colors);
+
+        Assert.assertArrayEquals(new int[]{0, 0, 1, 1, 2, 2}, colors);
+    }
+
+    @Test
+    public void test2() {
+        final int[] colors = {2, 0, 1};
+        final SortColors solution = new SortColors();
+        solution.sortColors(colors);
+
+        Assert.assertArrayEquals(new int[]{0, 1, 2}, colors);
+    }
+
+    @Test
+    public void test3() {
+        final int[] colors = {0};
+        final SortColors solution = new SortColors();
+        solution.sortColors(colors);
+
+        Assert.assertArrayEquals(new int[]{0}, colors);
+    }
+
+    @Test
+    public void test4() {
+        final int[] colors = {1};
+        final SortColors solution = new SortColors();
+        solution.sortColors(colors);
+
+        Assert.assertArrayEquals(new int[]{1}, colors);
+    }
+}
