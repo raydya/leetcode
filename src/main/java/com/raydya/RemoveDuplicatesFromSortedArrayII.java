e9db7c7c2a088d4fb9bd2523c4fb7e15a76aff85
zhanglei <dya.ilaempire@gmail.com>
2020-12-28 11:43:53 +0800

    optimization

--- src/main/java/com/raydya/RemoveDuplicatesFromSortedArrayII.java
+++ src/main/java/com/raydya/RemoveDuplicatesFromSortedArrayII.java
@@ -1,35 +1,20 @@
 package com.raydya;
 
-import java.util.Arrays;
-
 public class RemoveDuplicatesFromSortedArrayII {
     public int removeDuplicates(int[] nums) {
-        int cur = nums[0];
-        int cnt = 1;
-        int totalCnt = 0;
-        int i = 1;
-
-        while (i < nums.length - totalCnt) {
-            if (nums[i] == cur) {
-                cnt++;
-                if (cnt > 2) {
-                    totalCnt += cnt - 2;
-                    for (int j = i; j < nums.length; j++) {
-                        final int x = j - (cnt - 2);
-                        nums[x] = nums[j];
-                    }
-                    cnt--;
-                    i--;
-                }
+        int curr = nums[0], count = 0, actual_length = 0, length = 0;
+        for (int num : nums) {
+            actual_length++;
+            if (num == curr) {
+                count += 1;
+                if (count < 3) length++;
             } else {
-                cur = nums[i];
-                cnt = 1;
+                curr = num;
+                count = 1;
+                length++;
             }
-            i++;
+            if (length < actual_length) nums[length - 1] = num;
         }
-
-        nums = Arrays.copyOf(nums, nums.length - totalCnt);
-
-        return nums.length;
+        return length;
     }
 }
