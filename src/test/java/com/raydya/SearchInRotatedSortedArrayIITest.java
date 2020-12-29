06bd36a1d69018de022e745514ad78b8ff187269
zhanglei <dya.ilaempire@gmail.com>
2020-12-29 11:24:10 +0800

    update

---
+++ src/test/java/com/raydya/SearchInRotatedSortedArrayIITest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SearchInRotatedSortedArrayIITest {
+    @Test
+    public void test1() {
+        final SearchInRotatedSortedArrayII solution = new SearchInRotatedSortedArrayII();
+        final boolean output = solution.search(new int[]{2, 5, 6, 0, 0, 1, 2}, 0);
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final SearchInRotatedSortedArrayII solution = new SearchInRotatedSortedArrayII();
+        final boolean output = solution.search(new int[]{2, 5, 6, 0, 0, 1, 2}, 3);
+
+        Assert.assertFalse(output);
+    }
+}
