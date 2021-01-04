f106e46d8d5fdcef3ce6ba7e9a8ce8bef0cc51ec
zhanglei <dya.ilaempire@gmail.com>
2021-01-04 11:24:47 +0800

    reverse linked list ii

---
+++ src/test/java/com/raydya/ReverseLinkedListIITest.java
@@ -0,0 +1,25 @@
+package com.raydya;
+
+import com.raydya.data.type.ListNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReverseLinkedListIITest {
+    @Test
+    public void test1() {
+        final ListNode n5 = new ListNode(5);
+        final ListNode n4 = new ListNode(4, n5);
+        final ListNode n3 = new ListNode(3, n4);
+        final ListNode n2 = new ListNode(2, n3);
+        final ListNode head = new ListNode(1, n2);
+
+        final ReverseLinkedListII solution = new ReverseLinkedListII();
+        final ListNode node = solution.reverseBetween(head, 2, 4);
+
+        Assert.assertEquals(1, node.val);
+        Assert.assertEquals(4, node.next.val);
+        Assert.assertEquals(3, node.next.next.val);
+        Assert.assertEquals(2, node.next.next.next.val);
+        Assert.assertEquals(5, node.next.next.next.next.val);
+    }
+}
