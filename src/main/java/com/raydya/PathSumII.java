b6c3dd99e6c0496d6f8f4f58724c2440b31a63cf
zhanglei <dya.ilaempire@gmail.com>
2021-02-03 15:33:41 +0800

    path sum ii

--- src/main/java/com/raydya/PathSumII.java
+++ src/main/java/com/raydya/PathSumII.java
@@ -2,10 +2,27 @@ package com.raydya;
 
 import com.raydya.data.type.TreeNode;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PathSumII {
+    private List<List<Integer>> resultList = new ArrayList<>();
+
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        return null;
+        if (root == null) return resultList;
+        Stack<Integer> path = new Stack<>();
+        pathSumInner(root, targetSum, path);
+        return resultList;
+    }
+
+    public void pathSumInner(TreeNode root, int sum, Stack<Integer> path) {
+        path.push(root.val);
+        if (root.left == null && root.right == null && sum == root.val) {
+            resultList.add(new ArrayList<>(path));
+        }
+        if (root.left != null) pathSumInner(root.left, sum - root.val, path);
+        if (root.right != null) pathSumInner(root.right, sum - root.val, path);
+        path.pop();
     }
 }
--- src/test/java/com/raydya/PathSumIITest.java
+++ src/test/java/com/raydya/PathSumIITest.java
@@ -1,10 +1,45 @@
 package com.raydya;
 
+import com.raydya.data.type.TreeNode;
+import com.raydya.utils.TreeNodeUtil;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class PathSumIITest {
     @Test
     public void test1() {
+        List<Integer> vals = Arrays.asList(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, null, null, 5, 1);
+        final TreeNode root = TreeNodeUtil.build(vals);
 
+        final PathSumII solution = new PathSumII();
+        final List<List<Integer>> output = solution.pathSum(root, 22);
+
+        Assert.assertArrayEquals(new Integer[]{5, 4, 11, 2}, output.get(0).toArray());
+        Assert.assertArrayEquals(new Integer[]{5, 8, 4, 5}, output.get(1).toArray());
+    }
+
+    @Test
+    public void test2() {
+        final List<Integer> vals = Arrays.asList(1, 2, 3);
+        final TreeNode root = TreeNodeUtil.build(vals);
+
+        final PathSumII solution = new PathSumII();
+        final List<List<Integer>> output = solution.pathSum(root, 5);
+
+        Assert.assertEquals(0, output.size());
+    }
+
+    @Test
+    public void test3() {
+        final List<Integer> vals = Arrays.asList(1, 2);
+        final TreeNode root = TreeNodeUtil.build(vals);
+
+        final PathSumII solution = new PathSumII();
+        final List<List<Integer>> output = solution.pathSum(root, 0);
+
+        Assert.assertEquals(0, output.size());
     }
 }
