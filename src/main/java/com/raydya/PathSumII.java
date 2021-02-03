3ad56c5245e9c0cfe44f24233c61df4e263662b7
zhanglei <dya.ilaempire@gmail.com>
2021-02-03 15:35:13 +0800

    optimization

--- src/main/java/com/raydya/PathSumII.java
+++ src/main/java/com/raydya/PathSumII.java
@@ -4,25 +4,24 @@ import com.raydya.data.type.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
 
 public class PathSumII {
     private List<List<Integer>> resultList = new ArrayList<>();
 
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         if (root == null) return resultList;
-        Stack<Integer> path = new Stack<>();
+        List<Integer> path = new ArrayList<>();
         pathSumInner(root, targetSum, path);
         return resultList;
     }
 
-    public void pathSumInner(TreeNode root, int sum, Stack<Integer> path) {
-        path.push(root.val);
+    public void pathSumInner(TreeNode root, int sum, List<Integer> path) {
+        path.add(root.val);
         if (root.left == null && root.right == null && sum == root.val) {
             resultList.add(new ArrayList<>(path));
         }
         if (root.left != null) pathSumInner(root.left, sum - root.val, path);
         if (root.right != null) pathSumInner(root.right, sum - root.val, path);
-        path.pop();
+        path.remove(path.size() - 1);
     }
 }
