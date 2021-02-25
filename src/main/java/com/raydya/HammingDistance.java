256f2a395bd7eb5e79c9ec26c79d70da0931e8e3
zhanglei <dya.ilaempire@gmail.com>
2021-02-25 18:12:56 +0800

    count 1

--- src/main/java/com/raydya/HammingDistance.java
+++ src/main/java/com/raydya/HammingDistance.java
@@ -3,6 +3,8 @@ package com.raydya;
 public class HammingDistance {
     public int hammingDistance(int x, int y) {
         int i = x ^ y;
+
+        // count '1'
         int cnt = 0;
         while (i != 0) {
             cnt += i & 1;
