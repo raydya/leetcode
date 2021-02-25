db0c5add366fecf66ec9202a91d6408e97044e12
zhanglei <dya.ilaempire@gmail.com>
2021-02-25 14:17:24 +0800

    transpose matrix

---
+++ src/main/java/com/raydya/TransposeMatrix.java
@@ -0,0 +1,18 @@
+package com.raydya;
+
+public class TransposeMatrix {
+    public int[][] transpose(int[][] matrix) {
+        final int m = matrix.length;
+        final int n = matrix[0].length;
+
+        final int[][] h = new int[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                h[j][i] = matrix[i][j];
+            }
+        }
+
+        return h;
+    }
+}
---
+++ src/test/java/com/raydya/TransposeMatrixTest.java
@@ -0,0 +1,26 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TransposeMatrixTest {
+    @Test
+    public void test1() {
+        final TransposeMatrix solution = new TransposeMatrix();
+        final int[][] output = solution.transpose(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+        Assert.assertArrayEquals(new int[][]{
+            {1, 4, 7}, {2, 5, 8}, {3, 6, 9}
+        }, output);
+    }
+
+    @Test
+    public void test2() {
+        final TransposeMatrix solution = new TransposeMatrix();
+        final int[][] output = solution.transpose(new int[][]{{1, 2, 3}, {4, 5, 6}});
+
+        Assert.assertArrayEquals(new int[][]{
+            {1, 4}, {2, 5}, {3, 6}
+        }, output);
+    }
+}
