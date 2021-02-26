bfe2538d8277288181e9ab2c0099c4003626efe3
zhanglei <dya.ilaempire@gmail.com>
2021-02-26 10:59:40 +0800

    word break

---
+++ src/main/java/com/raydya/WordBreak.java
@@ -0,0 +1,23 @@
+package com.raydya;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                final String substring = s.substring(j, i);
+                if (dp[j] && wordDictSet.contains(substring)) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
