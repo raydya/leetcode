438e0fd800ea361b64e0893ec3518fc75411089e
zhanglei <dya.ilaempire@gmail.com>
2021-03-08 10:51:35 +0800

    number of islands

---
+++ src/main/java/com/raydya/NumberOfIslands.java
@@ -0,0 +1,37 @@
+package com.raydya;
+
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        final int r = grid.length;
+        final int c = grid[0].length;
+        final boolean[][] visited = new boolean[r][c];
+        int num = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                final char n = grid[i][j];
+                if (n == '1' && !visited[i][j]) {
+                    num++;
+                    markBorderConnectedOne(visited, grid, i, j, r, c);
+                }
+            }
+        }
+
+        return num;
+    }
+
+    private void markBorderConnectedOne(boolean[][] visited, char[][] grid, int i, int j, int r, int c) {
+        if (i < 0 || i >= r) return;
+        if (j < 0 || j >= c) return;
+
+        final char n = grid[i][j];
+        if (n == '0' || visited[i][j]) return;
+
+        visited[i][j] = true;
+
+        markBorderConnectedOne(visited, grid, i - 1, j, r, c);
+        markBorderConnectedOne(visited, grid, i + 1, j, r, c);
+        markBorderConnectedOne(visited, grid, i, j - 1, r, c);
+        markBorderConnectedOne(visited, grid, i, j + 1, r, c);
+    }
+}
---
+++ src/test/java/com/raydya/NumberOfIslandsTest.java
@@ -0,0 +1,34 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NumberOfIslandsTest {
+    @Test
+    public void test1() {
+        final char[][] grid = {
+            {'1', '1', '1', '1', '0'},
+            {'1', '1', '0', '1', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '0', '0', '0'}
+        };
+        final NumberOfIslands solution = new NumberOfIslands();
+        final int output = solution.numIslands(grid);
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test2() {
+        final char[][] grid = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+        final NumberOfIslands solution = new NumberOfIslands();
+        final int output = solution.numIslands(grid);
+
+        Assert.assertEquals(3, output);
+    }
+}
