6f67c3178d900fa82f231ab139d06d84521246bd
zhanglei <dya.ilaempire@gmail.com>
2021-03-10 10:26:04 +0800

    trie

---
+++ src/main/java/com/raydya/Trie.java
@@ -0,0 +1,86 @@
+package com.raydya;
+
+public class Trie {
+    public static class TrieNode {
+        private static final int R = 26;
+        private final TrieNode[] links;
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
+
+    private final TrieNode root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        TrieNode node = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            final char ch = word.charAt(i);
+            if (!node.containsKey(ch)) {
+                node.put(ch, new TrieNode());
+            }
+            node = node.get(ch);
+        }
+        node.setEnd();
+    }
+
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = this.root;
+        for (int i = 0; i < word.length(); i++) {
+            final char ch = word.charAt(i);
+            if (node.containsKey(ch)) {
+                node = node.get(ch);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        final TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        final TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
---
+++ src/test/java/com/raydya/TrieTest.java
@@ -0,0 +1,26 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TrieTest {
+    @Test
+    public void test1() {
+        final Trie trie = new Trie();
+
+        trie.insert("apple");
+        Assert.assertTrue(trie.search("apple"));
+        Assert.assertFalse(trie.search("app"));
+        Assert.assertTrue(trie.startsWith("app"));
+        trie.insert("app");
+        Assert.assertTrue(trie.search("app"));
+    }
+
+    @Test
+    public void test2() {
+        final Trie trie = new Trie();
+
+        trie.insert("");
+        Assert.assertFalse(trie.startsWith("a"));
+    }
+}
