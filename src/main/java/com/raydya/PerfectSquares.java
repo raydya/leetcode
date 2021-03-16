6b9e26496deb0e9774a38ec2376ff14cdc74c9f4
zhanglei <dya.ilaempire@gmail.com>
2021-03-16 15:25:45 +0800

    perfect squares

---
+++ src/main/java/com/raydya/PerfectSquares.java
@@ -0,0 +1,26 @@
+package com.raydya;
+
+public class PerfectSquares {
+    protected boolean isSquare(int n) {
+        int sq = (int) Math.sqrt(n);
+        return n == sq * sq;
+    }
+
+    public int numSquares(int n) {
+        // four-square and three-square theorems.
+        while (n % 4 == 0)
+            n /= 4;
+        if (n % 8 == 7)
+            return 4;
+
+        if (this.isSquare(n))
+            return 1;
+        // enumeration to check if the number can be decomposed into sum of two squares.
+        for (int i = 1; i * i <= n; ++i) {
+            if (this.isSquare(n - i * i))
+                return 2;
+        }
+        // bottom case of three-square theorem.
+        return 3;
+    }
+}
---
+++ src/test/java/com/raydya/PerfectSquaresTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PerfectSquaresTest {
+    @Test
+    public void test1() {
+        final PerfectSquares solution = new PerfectSquares();
+        final int output = solution.numSquares(12);
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test2() {
+        final PerfectSquares solution = new PerfectSquares();
+        final int output = solution.numSquares(13);
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test3() {
+        final PerfectSquares solution = new PerfectSquares();
+        final int output = solution.numSquares(43);
+
+        Assert.assertEquals(3, output);
+    }
+}
