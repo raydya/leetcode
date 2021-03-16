83c73ea327e14b5ef100e9f07f4648637e8689a4
zhanglei <dya.ilaempire@gmail.com>
2021-03-16 10:28:10 +0800

    product of array except self

---
+++ src/main/java/com/raydya/ProductOfArrayExceptSelf.java
@@ -0,0 +1,20 @@
+package com.raydya;
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+
+        answer[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int r = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * r;
+            r *= nums[i];
+        }
+        return answer;
+    }
+}
---
+++ src/test/java/com/raydya/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,14 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProductOfArrayExceptSelfTest {
+    @Test
+    public void test1() {
+        final ProductOfArrayExceptSelf solution = new ProductOfArrayExceptSelf();
+        final int[] output = solution.productExceptSelf(new int[]{1, 2, 3, 4});
+
+        Assert.assertArrayEquals(new int[]{24, 12, 8, 6}, output);
+    }
+}
