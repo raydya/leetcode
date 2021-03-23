c2f49ffb293ac0d94b738874871f0d4c43c5964b
zhanglei <dya.ilaempire@gmail.com>
2021-03-23 17:56:01 +0800

    queue reconstruction by height

---
+++ src/test/java/com/raydya/QueueReconstructionByHeightTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QueueReconstructionByHeightTest {
+    @Test
+    public void test1() {
+        final int[][] input = {
+            {7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}
+        };
+
+        final QueueReconstructionByHeight solution = new QueueReconstructionByHeight();
+        final int[][] output = solution.reconstructQueue(input);
+
+        Assert.assertArrayEquals(new int[][]{{5, 0}, {7, 0}, {6, 1}, {7, 1}, {4, 4}}, output);
+    }
+
+    @Test
+    public void test2() {
+        final int[][] input = {
+            {6, 0}, {5, 0}, {4, 0}, {3, 2}, {2, 2}
+        };
+
+        final QueueReconstructionByHeight solution = new QueueReconstructionByHeight();
+        final int[][] output = solution.reconstructQueue(input);
+
+        Assert.assertArrayEquals(new int[][]{{4, 0}, {5, 0}, {2, 2}, {3, 2}, {6, 0}}, output);
+    }
+}
