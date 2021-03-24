c982b8f094e11ff90b5f0d99d0a70e945cba5073
zhanglei <dya.ilaempire@gmail.com>
2021-03-24 15:15:57 +0800

    path sum iii

---
+++ src/main/java/com/raydya/PathSumIII.java
@@ -0,0 +1,45 @@
+package com.raydya;
+
+import com.raydya.data.type.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PathSumIII {
+    public int pathSum(TreeNode root, int sum) {
+        // key是前缀和, value是大小为key的前缀和出现的次数
+        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        // 前缀和为0的一条路径
+        prefixSumCount.put(0, 1);
+        // 前缀和的递归回溯思路
+        return recursionPathSum(root, prefixSumCount, sum, 0);
+    }
+
+    private int recursionPathSum(TreeNode node, Map<Integer, Integer> prefixSumCount, int target, int currSum) {
+        // 1.递归终止条件
+        if (node == null) {
+            return 0;
+        }
+        // 2.本层要做的事情
+        int res = 0;
+        // 当前路径上的和
+        currSum += node.val;
+
+        //---核心代码
+        // 看看root到当前节点这条路上是否存在节点前缀和加target为currSum的路径
+        // 当前节点->root节点反推，有且仅有一条路径，如果此前有和为currSum-target,而当前的和又为currSum,两者的差就肯定为target了
+        // currSum-target相当于找路径的起点，起点的sum+target=currSum，当前点到起点的距离就是target
+        res += prefixSumCount.getOrDefault(currSum - target, 0);
+        // 更新路径上当前节点前缀和的个数
+        prefixSumCount.put(currSum, prefixSumCount.getOrDefault(currSum, 0) + 1);
+        //---核心代码
+
+        // 3.进入下一层
+        res += recursionPathSum(node.left, prefixSumCount, target, currSum);
+        res += recursionPathSum(node.right, prefixSumCount, target, currSum);
+
+        // 4.回到本层，恢复状态，去除当前节点的前缀和数量
+        prefixSumCount.put(currSum, prefixSumCount.get(currSum) - 1);
+        return res;
+    }
+}
---
+++ src/test/java/com/raydya/PathSumIIITest.java
@@ -0,0 +1,40 @@
+package com.raydya;
+
+import com.raydya.data.type.TreeNode;
+import com.raydya.utils.TreeNodeUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class PathSumIIITest {
+    @Test
+    public void test1() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(10, 5, -3, 3, 2, null, 11, 3, -2, null, 1));
+
+        final PathSumIII solution = new PathSumIII();
+        final int output = solution.pathSum(root, 8);
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test2() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1));
+
+        final PathSumIII solution = new PathSumIII();
+        final int output = solution.pathSum(root, 22);
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test3() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(-2, null, -3));
+
+        final PathSumIII solution = new PathSumIII();
+        final int output = solution.pathSum(root, -5);
+
+        Assert.assertEquals(1, output);
+    }
+}
