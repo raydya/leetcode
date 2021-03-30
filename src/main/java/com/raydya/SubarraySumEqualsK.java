e5df25d947b0b1ffe87bdcc9ab62da59abc437d7
zhanglei <dya.ilaempire@gmail.com>
2021-03-30 14:54:21 +0800

    subarray sum equals k optimization

--- src/main/java/com/raydya/SubarraySumEqualsK.java
+++ src/main/java/com/raydya/SubarraySumEqualsK.java
@@ -1,17 +1,21 @@
 package com.raydya;
 
+import java.util.HashMap;
+
 public class SubarraySumEqualsK {
     public int subarraySum(int[] nums, int k) {
-        int cnt = 0;
-        for (int i = 0; i < nums.length; i++) {
-            int sum = nums[i];
-            if (sum == k) cnt++;
-            for (int j = i + 1; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum == k) cnt++;
+        int count = 0;
+        int pre = 0;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, 1);
+        for (final int num : nums) {
+            pre += num;
+            final int n = pre - k;
+            if (mp.containsKey(n)) {
+                count += mp.get(n);
             }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
         }
-
-        return cnt;
+        return count;
     }
 }
