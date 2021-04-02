e9d148f1e66658d26febb8da991c65c7add8a146
zhanglei <dya.ilaempire@gmail.com>
2021-04-02 17:28:01 +0800

    daily temperatures

---
+++ src/main/java/com/raydya/DailyTemperatures.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] T) {
+        final int[] d = new int[T.length];
+
+        for (int i = 0; i < T.length; i++) {
+            final int t1 = T[i];
+
+            for (int j = i + 1; j < T.length; j++) {
+                final int t2 = T[j];
+
+                if (t2 > t1) {
+                    d[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return d;
+    }
+}
---
+++ src/test/java/com/raydya/DailyTemperaturesTest.java
@@ -0,0 +1,14 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DailyTemperaturesTest {
+    @Test
+    public void test1() {
+        final DailyTemperatures solution = new DailyTemperatures();
+        final int[] output = solution.dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73});
+
+        Assert.assertArrayEquals(new int[]{1, 1, 4, 2, 1, 1, 0, 0}, output);
+    }
+}
