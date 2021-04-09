abdb7474178c7159b15efa43e592b6cefb3918f6
zhanglei <dya.ilaempire@gmail.com>
2021-04-09 17:47:49 +0800

    update

---
+++ src/main/java/com/raydya/FindPeakElement.java
@@ -0,0 +1,21 @@
+package com.raydya;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        int maxValue = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        final int l = nums.length;
+        for (int i = 0; i < l; i++) {
+            final int num = nums[i];
+            if (num > maxValue) {
+                maxValue = num;
+                maxIndex = i;
+            }
+            if (i == 0 || i == l - 1) continue;
+
+            if (num > nums[i - 1] && num > nums[i + 1]) return i;
+        }
+
+        return maxIndex;
+    }
+}
---
+++ src/test/java/com/raydya/FindPeakElementTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindPeakElementTest {
+    @Test
+    public void test1() {
+        final FindPeakElement solution = new FindPeakElement();
+        final int output = solution.findPeakElement(new int[]{1, 2, 3, 1});
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test2() {
+        final FindPeakElement solution = new FindPeakElement();
+        final int output = solution.findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4});
+
+        Assert.assertEquals(1, output);
+    }
+}
