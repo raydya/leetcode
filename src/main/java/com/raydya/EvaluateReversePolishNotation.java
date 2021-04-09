44f166794f3c7dee18ae0a2843998e882155e5dc
zhanglei <dya.ilaempire@gmail.com>
2021-04-09 15:33:16 +0800

    evaluate reverse polish notation

---
+++ src/main/java/com/raydya/EvaluateReversePolishNotation.java
@@ -0,0 +1,43 @@
+package com.raydya;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class EvaluateReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new LinkedList<>();
+
+        for (final String token : tokens) {
+            if (isSymbol(token)) {
+                final Integer p2 = stack.pop();
+                final Integer p1 = stack.pop();
+                Integer result = null;
+                switch (token) {
+                    case "+":
+                        result = p1 + p2;
+                        break;
+                    case "-":
+                        result = p1 - p2;
+                        break;
+                    case "*":
+                        result = p1 * p2;
+                        break;
+                    case "/":
+                        result = p1 / p2;
+                        break;
+                    default:
+                        break;
+                }
+                stack.push(result);
+            } else {
+                stack.push(Integer.valueOf(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isSymbol(String str) {
+        return str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/");
+    }
+}
---
+++ src/test/java/com/raydya/EvaluateReversePolishNotationTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EvaluateReversePolishNotationTest {
+    @Test
+    public void test1() {
+        final EvaluateReversePolishNotation solution = new EvaluateReversePolishNotation();
+        final int output = solution.evalRPN(new String[]{"2", "1", "+", "3", "*"});
+
+        Assert.assertEquals(9, output);
+    }
+
+    @Test
+    public void test2() {
+        final EvaluateReversePolishNotation solution = new EvaluateReversePolishNotation();
+        final int output = solution.evalRPN(new String[]{"4", "13", "5", "/", "+"});
+
+        Assert.assertEquals(6, output);
+    }
+
+    @Test
+    public void test3() {
+        final EvaluateReversePolishNotation solution = new EvaluateReversePolishNotation();
+        final int output = solution.evalRPN(new String[]{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"});
+
+        Assert.assertEquals(22, output);
+    }
+}
