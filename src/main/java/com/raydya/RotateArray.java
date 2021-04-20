b7e245b0cba6f229645ee5abd9356d159e7c0dd9
zhanglei <dya.ilaempire@gmail.com>
2021-04-20 11:31:10 +0800

    rotate array

---
+++ src/main/java/com/raydya/RotateArray.java
@@ -0,0 +1,15 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        final int l = nums.length;
+        if (l == 1) return;
+        final int[] copy = Arrays.copyOf(nums, l);
+        for (int i = 0; i < l; i++) {
+            final int m = i + k >= l ? (i + k) % l : i + k;
+            nums[m] = copy[i];
+        }
+    }
+}
---
+++ src/test/java/com/raydya/RotateArrayTest.java
@@ -0,0 +1,42 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RotateArrayTest {
+    @Test
+    public void test1() {
+        final RotateArray solution = new RotateArray();
+        final int[] input = {1, 2, 3, 4, 5, 6, 7};
+        solution.rotate(input, 3);
+
+        Assert.assertArrayEquals(new int[]{5, 6, 7, 1, 2, 3, 4}, input);
+    }
+
+    @Test
+    public void test2() {
+        final RotateArray solution = new RotateArray();
+        final int[] input = {-1, -100, 3, 99};
+        solution.rotate(input, 2);
+
+        Assert.assertArrayEquals(new int[]{3, 99, -1, -100}, input);
+    }
+
+    @Test
+    public void test3() {
+        final RotateArray solution = new RotateArray();
+        final int[] input = {-1};
+        solution.rotate(input, 2);
+
+        Assert.assertArrayEquals(new int[]{-1}, input);
+    }
+
+    @Test
+    public void test4() {
+        final RotateArray solution = new RotateArray();
+        final int[] input = {1, 2};
+        solution.rotate(input, 3);
+
+        Assert.assertArrayEquals(new int[]{2, 1}, input);
+    }
+}
