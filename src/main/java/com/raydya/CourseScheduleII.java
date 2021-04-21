b4617f8c140331da91bb945f012971f1b8fc5fad
zhanglei <dya.ilaempire@gmail.com>
2021-04-21 16:40:37 +0800

    num courses

---
+++ src/main/java/com/raydya/CourseScheduleII.java
@@ -0,0 +1,48 @@
+package com.raydya;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CourseScheduleII {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] inDegrees = new int[numCourses];
+        final List<List<Integer>> edges = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+
+        for (final int[] prerequisite : prerequisites) {
+            final int a = prerequisite[0];
+            final int b = prerequisite[1];
+            inDegrees[a]++;
+            edges.get(b).add(a);
+        }
+
+        final Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        final int[] result = new int[numCourses];
+        int index = 0;
+        while (!queue.isEmpty()) {
+            final Integer u = queue.poll();
+            result[index++] = u;
+            for (final Integer r : edges.get(u)) {
+                inDegrees[r]--;
+                if (inDegrees[r] == 0) {
+                    queue.offer(r);
+                }
+            }
+        }
+
+        if (index != numCourses) {
+            return new int[0];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/raydya/CourseScheduleIITest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CourseScheduleIITest {
+    @Test
+    public void test1() {
+        final CourseScheduleII solution = new CourseScheduleII();
+        final int[] output = solution.findOrder(2, new int[][]{{1, 0}});
+
+        Assert.assertArrayEquals(new int[]{0, 1}, output);
+    }
+
+    @Test
+    public void test2() {
+        final CourseScheduleII solution = new CourseScheduleII();
+        final int[] output = solution.findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}});
+
+        Assert.assertArrayEquals(new int[]{0, 1, 2, 3}, output);
+    }
+}
