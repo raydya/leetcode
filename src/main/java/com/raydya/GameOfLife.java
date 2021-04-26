9d417055529550c0781876698d512030fd9aa466
zhanglei <dya.ilaempire@gmail.com>
2021-04-26 11:17:43 +0800

    game of life

---
+++ src/main/java/com/raydya/GameOfLife.java
@@ -0,0 +1,45 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class GameOfLife {
+    public void gameOfLife(int[][] board) {
+        final int[][] copy = Arrays.stream(board)
+            .map(int[]::clone)
+            .toArray(int[][]::new);
+
+        final int m = copy.length;
+        final int n = copy[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                final int cnt = countSurroundingAlive(copy, i, j);
+
+                if ((cnt < 2 || cnt > 3) && copy[i][j] == 1) {
+                    board[i][j] = 0;
+                } else if (cnt == 3 && copy[i][j] == 0) {
+                    board[i][j] = 1;
+                } else if (copy[i][j] == 1) {
+                    board[i][j] = 1;
+                }
+            }
+        }
+    }
+
+    public int countSurroundingAlive(int[][] board, int m, int n) {
+        final int k = board.length;
+        final int h = board[0].length;
+        int cnt = 0;
+
+        for (int i = m - 1; i <= m + 1; i++) {
+            if (i < 0 || i >= k) continue;
+            for (int j = n - 1; j <= n + 1; j++) {
+                if (j < 0 || j >= h) continue;
+                if (i == m && j == n) continue;
+                cnt += board[i][j];
+            }
+        }
+
+        return cnt;
+    }
+}
---
+++ src/test/java/com/raydya/GameOfLifeTest.java
@@ -0,0 +1,66 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameOfLifeTest {
+    @Test
+    public void testCount1() {
+        final int[][] board = {{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}};
+
+        final GameOfLife solution = new GameOfLife();
+        final int output = solution.countSurroundingAlive(board, 1, 2);
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void testCount2() {
+        final int[][] board = {{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}};
+
+        final GameOfLife solution = new GameOfLife();
+        final int output = solution.countSurroundingAlive(board, 1, 0);
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test1() {
+        final int[][] board = {{0, 1, 0}, {0, 0, 1}, {1, 1, 1}, {0, 0, 0}};
+
+        final GameOfLife solution = new GameOfLife();
+        solution.gameOfLife(board);
+
+        Assert.assertArrayEquals(
+            new int[]{0, 0, 0}, board[0]
+        );
+
+        Assert.assertArrayEquals(
+            new int[]{1, 0, 1}, board[1]
+        );
+
+        Assert.assertArrayEquals(
+            new int[]{0, 1, 1}, board[2]
+        );
+
+        Assert.assertArrayEquals(
+            new int[]{0, 1, 0}, board[3]
+        );
+    }
+
+    @Test
+    public void test2() {
+        final int[][] board = {{1, 1}, {1, 0}};
+
+        final GameOfLife solution = new GameOfLife();
+        solution.gameOfLife(board);
+
+        Assert.assertArrayEquals(
+            new int[]{1, 1}, board[0]
+        );
+
+        Assert.assertArrayEquals(
+            new int[]{1, 1}, board[1]
+        );
+    }
+}
