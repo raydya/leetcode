34498069021b7b1296a8ebfea5cfc11cce5cf042
zhanglei <dya.ilaempire@gmail.com>
2021-04-27 10:48:18 +0800

    wiggle sort

---
+++ src/main/java/com/raydya/WiggleSort.java
@@ -0,0 +1,21 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class WiggleSort {
+    public void wiggleSort(int[] nums) {
+        //排序
+        Arrays.sort(nums);
+        int len = nums.length, i = 0;
+        int[] smaller = new int[len % 2 == 0 ? len / 2 : (len / 2 + 1)], bigger = new int[len / 2];
+        //复制
+        System.arraycopy(nums, 0, smaller, 0, smaller.length);
+        System.arraycopy(nums, smaller.length, bigger, 0, len / 2);
+        //穿插
+        for (; i < len / 2; i++) {
+            nums[2 * i] = smaller[smaller.length - 1 - i];
+            nums[2 * i + 1] = bigger[len / 2 - 1 - i];
+        }
+        if (len % 2 != 0) nums[2 * i] = smaller[smaller.length - 1 - i];
+    }
+}
---
+++ src/test/java/com/raydya/WiggleSortTest.java
@@ -0,0 +1,26 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WiggleSortTest {
+    @Test
+    public void test1() {
+        final int[] nums = {1, 5, 1, 1, 6, 4};
+
+        final WiggleSort solution = new WiggleSort();
+        solution.wiggleSort(nums);
+
+        Assert.assertArrayEquals(new int[]{1, 6, 1, 5, 1, 4}, nums);
+    }
+
+    @Test
+    public void test2() {
+        final int[] nums = {1, 3, 2, 2, 3, 1};
+
+        final WiggleSort solution = new WiggleSort();
+        solution.wiggleSort(nums);
+
+        Assert.assertArrayEquals(new int[]{2, 3, 1, 3, 1, 2}, nums);
+    }
+}
