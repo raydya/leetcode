cc80b85f4bf06c79ecc959e30d8f5548715efeb3
zhanglei <dya.ilaempire@gmail.com>
2021-05-11 19:01:58 +0800

    kth smallest element in a sorted matrix

---
+++ src/main/java/com/raydya/KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,24 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class KthSmallestElementInASortedMatrix {
+    public int kthSmallest(int[][] matrix, int k) {
+        final int m = matrix.length;
+        final int n = matrix[0].length;
+
+        final int[] ints = new int[m * n];
+        int l = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ints[l] = matrix[i][j];
+                l++;
+            }
+        }
+
+        Arrays.sort(ints);
+
+        return ints[k - 1];
+    }
+}
---
+++ src/test/java/com/raydya/KthSmallestElementInASortedMatrixTest.java
@@ -0,0 +1,37 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class KthSmallestElementInASortedMatrixTest {
+    @Test
+    public void test1() {
+        final int[][] matrix = {new int[]{1, 5, 9}, new int[]{10, 11, 13}, new int[]{12, 13, 15}};
+
+        final KthSmallestElementInASortedMatrix solution = new KthSmallestElementInASortedMatrix();
+        final int output = solution.kthSmallest(matrix, 8);
+
+        Assert.assertEquals(13, output);
+    }
+
+    @Test
+    public void test2() {
+        final int[][] matrix = {new int[]{-5}};
+
+        final KthSmallestElementInASortedMatrix solution = new KthSmallestElementInASortedMatrix();
+        final int output = solution.kthSmallest(matrix, 1);
+
+        Assert.assertEquals(-5, output);
+    }
+
+
+    @Test
+    public void test3() {
+        final int[][] matrix = {new int[]{1, 2}, new int[]{1, 3}};
+
+        final KthSmallestElementInASortedMatrix solution = new KthSmallestElementInASortedMatrix();
+        final int output = solution.kthSmallest(matrix, 2);
+
+        Assert.assertEquals(1, output);
+    }
+}
