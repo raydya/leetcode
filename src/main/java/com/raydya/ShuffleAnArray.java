aa779c75b864f01a45c7fb02a10e800b2724d7ee
zhanglei <dya.ilaempire@gmail.com>
2021-05-13 16:35:04 +0800

    shuffle an array

---
+++ src/main/java/com/raydya/ShuffleAnArray.java
@@ -0,0 +1,38 @@
+package com.raydya;
+
+import java.util.Random;
+
+public class ShuffleAnArray {
+    private int[] array;
+    private int[] original;
+
+    Random rand = new Random();
+
+    private int randRange(int min, int max) {
+        return rand.nextInt(max - min) + min;
+    }
+
+    private void swapAt(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public ShuffleAnArray(int[] nums) {
+        array = nums;
+        original = nums.clone();
+    }
+
+    public int[] reset() {
+        array = original;
+        original = original.clone();
+        return original;
+    }
+
+    public int[] shuffle() {
+        for (int i = 0; i < array.length; i++) {
+            swapAt(i, randRange(i, array.length));
+        }
+        return array;
+    }
+}
