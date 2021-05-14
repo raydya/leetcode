e3ba07d74af1d3e023e9d1d4b7bf0195e7f82023
zhanglei <dya.ilaempire@gmail.com>
2021-05-14 18:49:46 +0800

    four sum ii

---
+++ src/main/java/com/raydya/FourSumII.java
@@ -0,0 +1,24 @@
+package com.raydya;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumII {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> countAB = new HashMap<>();
+        for (int u : nums1) {
+            for (int v : nums2) {
+                countAB.put(u + v, countAB.getOrDefault(u + v, 0) + 1);
+            }
+        }
+        int ans = 0;
+        for (int u : nums3) {
+            for (int v : nums4) {
+                if (countAB.containsKey(-u - v)) {
+                    ans += countAB.get(-u - v);
+                }
+            }
+        }
+        return ans;
+    }
+}
