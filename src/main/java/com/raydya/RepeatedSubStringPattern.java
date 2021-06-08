22b19e524f05f73a0a3e901b688c18d5c4efe32e
zhanglei <dya.ilaempire@gmail.com>
2021-06-08 11:53:41 +0800

    repeated substring pattern

---
+++ src/main/java/com/raydya/RepeatedSubStringPattern.java
@@ -0,0 +1,26 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class RepeatedSubStringPattern {
+    public boolean repeatedSubstringPattern(String s) {
+        return (s + s).indexOf(s, 1) != s.length();
+//        return kmp(s);
+    }
+
+    public boolean kmp(String pattern) {
+        int n = pattern.length();
+        int[] fail = new int[n];
+        Arrays.fill(fail, -1);
+        for (int i = 1; i < n; ++i) {
+            int j = fail[i - 1];
+            while (j != -1 && pattern.charAt(j + 1) != pattern.charAt(i)) {
+                j = fail[j];
+            }
+            if (pattern.charAt(j + 1) == pattern.charAt(i)) {
+                fail[i] = j + 1;
+            }
+        }
+        return fail[n - 1] != -1 && n % (n - fail[n - 1] - 1) == 0;
+    }
+}
---
+++ src/test/java/com/raydya/RepeatedSubstringPatternTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RepeatedSubstringPatternTest {
+    @Test
+    public void test1() {
+        final RepeatedSubStringPattern solution = new RepeatedSubStringPattern();
+        final boolean output = solution.repeatedSubstringPattern("abab");
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final RepeatedSubStringPattern solution = new RepeatedSubStringPattern();
+        final boolean output = solution.repeatedSubstringPattern("aba");
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test3() {
+        final RepeatedSubStringPattern solution = new RepeatedSubStringPattern();
+        final boolean output = solution.repeatedSubstringPattern("abcabcabcabc");
+
+        Assert.assertTrue(output);
+    }
+}
