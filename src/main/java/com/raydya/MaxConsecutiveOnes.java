c72433f0e8604c489e6f627958a5d8cac3b915f4
zhanglei <dya.ilaempire@gmail.com>
2021-06-15 11:44:18 +0800

    max consecutive ones

---
+++ src/main/java/com/raydya/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int[] h = new int[nums.length];
+        h[0] = nums[0];
+
+        int max = h[0];
+        for (int i = 1; i < nums.length; i++) {
+            final int n = nums[i];
+            if (n == 0) {
+                h[i] = 0;
+            } else {
+                h[i] += h[i - 1] + 1;
+            }
+
+            max = Math.max(max, h[i]);
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/com/raydya/MaxConsecutiveOnesTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxConsecutiveOnesTest {
+    @Test
+    public void test1() {
+        final MaxConsecutiveOnes solution = new MaxConsecutiveOnes();
+        final int output = solution.findMaxConsecutiveOnes(new int[]{1, 1, 0, 1, 1, 1});
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test2() {
+        final MaxConsecutiveOnes solution = new MaxConsecutiveOnes();
+        final int output = solution.findMaxConsecutiveOnes(new int[]{1, 0, 1, 1, 0, 1});
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test3() {
+        final MaxConsecutiveOnes solution = new MaxConsecutiveOnes();
+        final int output = solution.findMaxConsecutiveOnes(new int[]{1});
+
+        Assert.assertEquals(1, output);
+    }
+}
