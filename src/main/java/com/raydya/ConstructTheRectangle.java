84df88d5b203b674713989c1c3e946fe5053be72
zhanglei <dya.ilaempire@gmail.com>
2021-06-16 14:20:13 +0800

    construct rectangle

---
+++ src/main/java/com/raydya/ConstructTheRectangle.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+public class ConstructTheRectangle {
+    public int[] constructRectangle(int area) {
+        final double sqrt = Math.sqrt(area);
+        int l = (int) sqrt;
+        int w = (int) sqrt;
+        int g = Integer.MAX_VALUE;
+
+        for (int i = w; i > 0; i--) {
+            final int tl = area / i;
+            final int r = area % i;
+            if (r == 0 && g > tl - w) {
+                w = i;
+                l = tl;
+                g = l - w;
+            }
+        }
+
+        return new int[]{l, w};
+    }
+}
---
+++ src/test/java/com/raydya/ConstructTheRectangleTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConstructTheRectangleTest {
+    @Test
+    public void test1() {
+        final ConstructTheRectangle solution = new ConstructTheRectangle();
+        final int[] output = solution.constructRectangle(4);
+
+        Assert.assertArrayEquals(new int[]{2, 2}, output);
+    }
+
+    @Test
+    public void test2() {
+        final ConstructTheRectangle solution = new ConstructTheRectangle();
+        final int[] output = solution.constructRectangle(9);
+
+        Assert.assertArrayEquals(new int[]{3, 3}, output);
+    }
+
+    @Test
+    public void test3() {
+        final ConstructTheRectangle solution = new ConstructTheRectangle();
+        final int[] output = solution.constructRectangle(7);
+
+        Assert.assertArrayEquals(new int[]{7, 1}, output);
+    }
+
+    @Test
+    public void test4() {
+        final ConstructTheRectangle solution = new ConstructTheRectangle();
+        final int[] output = solution.constructRectangle(121);
+
+        Assert.assertArrayEquals(new int[]{11, 11}, output);
+    }
+
+    @Test
+    public void test5() {
+        final ConstructTheRectangle solution = new ConstructTheRectangle();
+        final int[] output = solution.constructRectangle(24);
+
+        Assert.assertArrayEquals(new int[]{6, 4}, output);
+    }
+}
