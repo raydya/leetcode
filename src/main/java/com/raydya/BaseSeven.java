f569d9972cd770f18d01475863bdefb68430db4d
zhanglei <dya.ilaempire@gmail.com>
2021-06-17 18:32:45 +0800

    updat

--- src/main/java/com/raydya/BaseSeven.java
+++ src/main/java/com/raydya/BaseSeven.java
@@ -4,14 +4,14 @@ public class BaseSeven {
     public String convertToBase7(int num) {
         int quotient = num / 7;
         int remainder = num % 7;
-        String ret = String.valueOf(Math.abs(remainder));
+        StringBuilder ret = new StringBuilder(String.valueOf(Math.abs(remainder)));
 
         while (quotient != 0) {
             remainder = quotient % 7;
             quotient = quotient / 7;
-            ret = Math.abs(remainder) + ret;
+            ret.insert(0, Math.abs(remainder));
         }
 
-        return num < 0 ? "-" + ret : ret;
+        return num < 0 ? "-" + ret : ret.toString();
     }
 }
