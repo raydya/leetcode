7c45ddf75dfbc523c8b14bd72384f20518223341
zhanglei <dya.ilaempire@gmail.com>
2021-06-18 16:38:28 +0800

    fibonacci number

---
+++ src/test/java/com/raydya/FibonacciNumberTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FibonacciNumberTest {
+    @Test
+    public void test1() {
+        final FibonacciNumber solution = new FibonacciNumber();
+        final int output = solution.fib(2);
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test2() {
+        final FibonacciNumber solution = new FibonacciNumber();
+        final int output = solution.fib(3);
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test3() {
+        final FibonacciNumber solution = new FibonacciNumber();
+        final int output = solution.fib(4);
+
+        Assert.assertEquals(3, output);
+    }
+}
