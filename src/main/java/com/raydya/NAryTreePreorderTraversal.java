322b47b9cc8e1093cc2137076724ed4f7f35a9fe
zhanglei <dya.ilaempire@gmail.com>
2021-06-23 17:40:12 +0800

    update

--- src/main/java/com/raydya/NAryTreePreorderTraversal.java
+++ src/main/java/com/raydya/NAryTreePreorderTraversal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NAryTreePreorderTraversal {
-    private List<Integer> ret = new ArrayList<>();
+    private final List<Integer> ret = new ArrayList<>();
 
     public List<Integer> preorder(Node root) {
         recursion(root);
