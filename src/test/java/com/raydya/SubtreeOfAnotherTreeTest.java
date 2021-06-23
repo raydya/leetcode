9ac108a13d0fa3513f96d9ab5e6dfbf213051ef4
zhanglei <dya.ilaempire@gmail.com>
2021-06-23 16:18:04 +0800

    subtree of another tree

---
+++ src/test/java/com/raydya/SubtreeOfAnotherTreeTest.java
@@ -0,0 +1,55 @@
+package com.raydya;
+
+import com.raydya.data.type.TreeNode;
+import com.raydya.utils.TreeNodeUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SubtreeOfAnotherTreeTest {
+    @Test
+    public void test1() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(3, 4, 5, 1, 2));
+        final TreeNode subRoot = TreeNodeUtil.build(Arrays.asList(4, 1, 2));
+
+        final SubtreeOfAnotherTree solution = new SubtreeOfAnotherTree();
+        final boolean output = solution.isSubtree(root, subRoot);
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(3, 4, 5, 1, 2, null, null, null, null, 0));
+        final TreeNode subRoot = TreeNodeUtil.build(Arrays.asList(4, 1, 2));
+
+        final SubtreeOfAnotherTree solution = new SubtreeOfAnotherTree();
+        final boolean output = solution.isSubtree(root, subRoot);
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test3() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(1, 1));
+        final TreeNode subRoot = TreeNodeUtil.build(Collections.singletonList(1));
+
+        final SubtreeOfAnotherTree solution = new SubtreeOfAnotherTree();
+        final boolean output = solution.isSubtree(root, subRoot);
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test4() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(3, 4, 5, 1, null, 2));
+        final TreeNode subRoot = TreeNodeUtil.build(Arrays.asList(3, 1, 2));
+
+        final SubtreeOfAnotherTree solution = new SubtreeOfAnotherTree();
+        final boolean output = solution.isSubtree(root, subRoot);
+
+        Assert.assertFalse(output);
+    }
+}
