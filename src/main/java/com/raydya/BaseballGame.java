398e7e80ef03190d0e6e11aee67d4f712759a9ae
zhanglei <dya.ilaempire@gmail.com>
2021-07-07 11:00:42 +0800

    update

---
+++ src/main/java/com/raydya/BaseballGame.java
@@ -0,0 +1,29 @@
+package com.raydya;
+
+import java.util.Stack;
+
+public class BaseballGame {
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack<>();
+        for (String op : ops) {
+            switch (op) {
+                case "C":
+                    stack.pop();
+                    break;
+                case "D":
+                    Integer calPointsDPeek = stack.peek();
+                    stack.push(calPointsDPeek * 2);
+                    break;
+                case "+":
+                    Integer calPointsAddPop = stack.pop();
+                    Integer calPointsAddPeek = stack.peek();
+                    stack.push(calPointsAddPop);
+                    stack.push(calPointsAddPop + calPointsAddPeek);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(op));
+            }
+        }
+        return stack.stream().reduce(Integer::sum).orElse(0);
+    }
+}
---
+++ src/test/java/com/raydya/BaseballGameTest.java
@@ -0,0 +1,38 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BaseballGameTest {
+    @Test
+    public void test1() {
+        final BaseballGame solution = new BaseballGame();
+        final int output = solution.calPoints(new String[]{"5", "2", "C", "D", "+"});
+
+        Assert.assertEquals(30, output);
+    }
+
+    @Test
+    public void test2() {
+        final BaseballGame solution = new BaseballGame();
+        final int output = solution.calPoints(new String[]{"5", "-2", "4", "C", "D", "9", "+", "+"});
+
+        Assert.assertEquals(27, output);
+    }
+
+    @Test
+    public void test3() {
+        final BaseballGame solution = new BaseballGame();
+        final int output = solution.calPoints(new String[]{"1"});
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test4() {
+        final BaseballGame solution = new BaseballGame();
+        final int output = solution.calPoints(new String[]{"57", "D", "-3", "-58", "D", "77", "+", "C", "+", "+", "38", "78", "-6", "24", "-46", "+", "31", "20", "D", "-81"});
+
+        Assert.assertEquals(146, output);
+    }
+}
