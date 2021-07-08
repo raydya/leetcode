7ba18655f11f22a51715002cc70391e593079caf
zhanglei <dya.ilaempire@gmail.com>
2021-07-08 14:11:49 +0800

    employee importance

---
+++ src/main/java/com/raydya/EmployeeImportance.java
@@ -0,0 +1,41 @@
+package com.raydya;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class EmployeeImportance {
+    private Map<Integer, Employee> map;
+    private int importance = 0;
+
+    public int getImportance(List<Employee> employees, int id) {
+        this.map = employees.stream().collect(
+            Collectors.toMap(
+                e -> e.id,
+                e -> e
+            )
+        );
+
+        final Employee employee = map.get(id);
+        importance += employee.importance;
+        final List<Integer> subordinates = employee.subordinates;
+        iterator(subordinates);
+
+        return importance;
+    }
+
+    private void iterator(List<Integer> subordinates) {
+        if (subordinates.isEmpty()) return;
+        for (final Integer subordinate : subordinates) {
+            final Employee e = map.get(subordinate);
+            importance += e.importance;
+            iterator(e.subordinates);
+        }
+    }
+
+    public static class Employee {
+        public int id;
+        public int importance;
+        public List<Integer> subordinates;
+    }
+}
---
+++ src/test/java/com/raydya/EmployeeImportanceTest.java
@@ -0,0 +1,67 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class EmployeeImportanceTest {
+    @Test
+    public void test1() {
+        final EmployeeImportance.Employee e1 = new EmployeeImportance.Employee();
+        e1.id = 1;
+        e1.importance = 5;
+        e1.subordinates = Arrays.asList(2, 3);
+
+
+        final EmployeeImportance.Employee e2 = new EmployeeImportance.Employee();
+        e2.id = 2;
+        e2.importance = 3;
+        e2.subordinates = Collections.emptyList();
+
+        final EmployeeImportance.Employee e3 = new EmployeeImportance.Employee();
+        e3.id = 3;
+        e3.importance = 3;
+        e3.subordinates = Collections.emptyList();
+
+        final List<EmployeeImportance.Employee> employees = Arrays.asList(e1, e2, e3);
+
+        final EmployeeImportance solution = new EmployeeImportance();
+        final int output = solution.getImportance(employees, 1);
+
+        Assert.assertEquals(11, output);
+    }
+
+    @Test
+    public void test2() {
+        final EmployeeImportance.Employee e1 = new EmployeeImportance.Employee();
+        e1.id = 1;
+        e1.importance = 5;
+        e1.subordinates = Arrays.asList(2, 3);
+
+
+        final EmployeeImportance.Employee e2 = new EmployeeImportance.Employee();
+        e2.id = 2;
+        e2.importance = 3;
+        e2.subordinates = Collections.singletonList(4);
+
+        final EmployeeImportance.Employee e3 = new EmployeeImportance.Employee();
+        e3.id = 3;
+        e3.importance = 4;
+        e3.subordinates = Collections.emptyList();
+
+        final EmployeeImportance.Employee e4 = new EmployeeImportance.Employee();
+        e4.id = 4;
+        e4.importance = 1;
+        e4.subordinates = Collections.emptyList();
+
+        final List<EmployeeImportance.Employee> employees = Arrays.asList(e1, e2, e3, e4);
+
+        final EmployeeImportance solution = new EmployeeImportance();
+        final int output = solution.getImportance(employees, 1);
+
+        Assert.assertEquals(13, output);
+    }
+}
