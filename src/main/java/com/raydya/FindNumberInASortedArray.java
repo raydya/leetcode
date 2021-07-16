9e0487d7a2c95a8ea34b2d8f1a800a9fbd79deaa
zhanglei <dya.ilaempire@gmail.com>
2021-07-16 18:22:20 +0800

    find number in a sorted array

---
+++ src/main/java/com/raydya/FindNumberInASortedArray.java
@@ -0,0 +1,24 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class FindNumberInASortedArray {
+    public int search(int[] nums, int target) {
+        final int i = Arrays.binarySearch(nums, target);
+        if (i < 0) {
+            return 0;
+        }
+        int ret = 0;
+        for (int j = i; j < nums.length; j++) {
+            if (nums[j] == target) ret++;
+            if (nums[j] > target) break;
+        }
+
+        for (int j = i - 1; j >= 0; j--) {
+            if (nums[j] == target) ret++;
+            if (nums[j] < target) break;
+        }
+
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/FindNumberInASortedArrayTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindNumberInASortedArrayTest {
+    @Test
+    public void test1() {
+        final FindNumberInASortedArray solution = new FindNumberInASortedArray();
+        final int output = solution.search(new int[]{5, 7, 7, 8, 8, 10}, 8);
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test2() {
+        final FindNumberInASortedArray solution = new FindNumberInASortedArray();
+        final int output = solution.search(new int[]{5, 7, 7, 8, 8, 10}, 6);
+
+        Assert.assertEquals(0, output);
+    }
+
+    @Test
+    public void test3() {
+        final FindNumberInASortedArray solution = new FindNumberInASortedArray();
+        final int output = solution.search(new int[]{5, 7, 7, 8, 8, 8, 10}, 8);
+
+        Assert.assertEquals(3, output);
+    }
+}
