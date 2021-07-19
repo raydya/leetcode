0a5aab3ee6e79c2848dde20dd8e2eca921aaea69
zhanglei <dya.ilaempire@gmail.com>
2021-07-19 10:14:19 +0800

    search in a binary search tree

---
+++ src/main/java/com/raydya/SearchInABinarySearchTree.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import com.raydya.data.type.TreeNode;
+
+public class SearchInABinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode node = root;
+
+        while (node != null) {
+            if (node.val == val) {
+                break;
+            }
+            if (node.val > val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+
+        return node;
+    }
+}
---
+++ src/test/java/com/raydya/SearchInABinarySearchTreeTest.java
@@ -0,0 +1,32 @@
+package com.raydya;
+
+import com.raydya.data.type.TreeNode;
+import com.raydya.utils.TreeNodeUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class SearchInABinarySearchTreeTest {
+    @Test
+    public void test1() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(4, 2, 7, 1, 3));
+
+        final SearchInABinarySearchTree solution = new SearchInABinarySearchTree();
+        final TreeNode output = solution.searchBST(root, 2);
+
+        Assert.assertEquals(2, output.val);
+        Assert.assertEquals(1, output.left.val);
+        Assert.assertEquals(3, output.right.val);
+    }
+
+    @Test
+    public void test2() {
+        final TreeNode root = TreeNodeUtil.build(Arrays.asList(4, 2, 7, 1, 3));
+
+        final SearchInABinarySearchTree solution = new SearchInABinarySearchTree();
+        final TreeNode output = solution.searchBST(root, 5);
+
+        Assert.assertNull(output);
+    }
+}
