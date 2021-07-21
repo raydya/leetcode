c498d617f3243d640122767cfb95879201987907
zhanglei <dya.ilaempire@gmail.com>
2021-07-21 14:07:55 +0800

    binary search

---
+++ src/main/java/com/raydya/BinarySearch.java
@@ -0,0 +1,25 @@
+package com.raydya;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums.length == 1 && nums[0] == target) return 0;
+        int s = 0;
+        int e = nums.length - 1;
+        int i = (s + e) / 2;
+
+        while (e > s && e - s > 1) {
+            if (target == nums[i]) return i;
+            if (target > nums[i]) {
+                s = i;
+            } else {
+                e = i;
+            }
+            i = (s + e) / 2;
+        }
+
+        if (nums[s] == target) return s;
+        if (nums[e] == target) return e;
+
+        return -1;
+    }
+}
---
+++ src/test/java/com/raydya/BinarySearchTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinarySearchTest {
+    @Test
+    public void test1() {
+        final BinarySearch solution = new BinarySearch();
+        final int output = solution.search(new int[]{-1, 0, 3, 5, 9, 12}, 9);
+
+        Assert.assertEquals(4, output);
+    }
+
+    @Test
+    public void test2() {
+        final BinarySearch solution = new BinarySearch();
+        final int output = solution.search(new int[]{-1, 0, 3, 5, 9, 12}, 2);
+
+        Assert.assertEquals(-1, output);
+    }
+
+    @Test
+    public void test3() {
+        final BinarySearch solution = new BinarySearch();
+        final int output = solution.search(new int[]{5}, 5);
+
+        Assert.assertEquals(0, output);
+    }
+
+    @Test
+    public void test4() {
+        final BinarySearch solution = new BinarySearch();
+        final int output = solution.search(new int[]{5}, -5);
+
+        Assert.assertEquals(-1, output);
+    }
+
+    @Test
+    public void test5() {
+        final BinarySearch solution = new BinarySearch();
+        final int output = solution.search(new int[]{2, 5}, 5);
+
+        Assert.assertEquals(1, output);
+    }
+}
