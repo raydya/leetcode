177fe2dae3f7bab738b6678f34bf2b045ff35272
zhanglei <dya.ilaempire@gmail.com>
2021-07-22 10:21:29 +0800

    my hash se

---
+++ src/main/java/com/raydya/MyHashSet.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyHashSet {
+    private final Map<Integer, Boolean> map;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyHashSet() {
+        this.map = new HashMap<>();
+    }
+
+    public void add(int key) {
+        map.putIfAbsent(key, true);
+    }
+
+    public void remove(int key) {
+        map.remove(key);
+    }
+
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        return map.containsKey(key);
+    }
+}
---
+++ src/test/java/com/raydya/MyHashSetTest.java
@@ -0,0 +1,19 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyHashSetTest {
+    @Test
+    public void test1() {
+        final MyHashSet myHashSet = new MyHashSet();
+        myHashSet.add(1);
+        myHashSet.add(2);
+        Assert.assertTrue(myHashSet.contains(1));
+        Assert.assertFalse(myHashSet.contains(3));
+        myHashSet.add(2);
+        Assert.assertTrue(myHashSet.contains(2));
+        myHashSet.remove(2);
+        Assert.assertFalse(myHashSet.contains(2));
+    }
+}
