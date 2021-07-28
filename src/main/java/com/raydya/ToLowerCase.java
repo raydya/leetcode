780ebfb9960a8e3738e75ed414dd9f361e55cd83
zhanglei <dya.ilaempire@gmail.com>
2021-07-28 10:48:46 +0800

    to lower case

---
+++ src/main/java/com/raydya/ToLowerCase.java
@@ -0,0 +1,18 @@
+package com.raydya;
+
+public class ToLowerCase {
+    private final char[] holder = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    public String toLowerCase(String s) {
+        final char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            final char aChar = chars[i];
+            final int offset = aChar - 'A';
+            if (offset >= 0 && offset <= 25) {
+                chars[i] = holder[offset];
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+}
---
+++ src/test/java/com/raydya/ToLowerCaseTest.java
@@ -0,0 +1,38 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ToLowerCaseTest {
+    @Test
+    public void test1() {
+        final ToLowerCase solution = new ToLowerCase();
+        final String output = solution.toLowerCase("Hello");
+
+        Assert.assertEquals("hello", output);
+    }
+
+    @Test
+    public void test2() {
+        final ToLowerCase solution = new ToLowerCase();
+        final String output = solution.toLowerCase("here");
+
+        Assert.assertEquals("here", output);
+    }
+
+    @Test
+    public void test3() {
+        final ToLowerCase solution = new ToLowerCase();
+        final String output = solution.toLowerCase("LOVELY");
+
+        Assert.assertEquals("lovely", output);
+    }
+
+    @Test
+    public void test4() {
+        final ToLowerCase solution = new ToLowerCase();
+        final String output = solution.toLowerCase("Hello@[]");
+
+        Assert.assertEquals("hello@[]", output);
+    }
+}
