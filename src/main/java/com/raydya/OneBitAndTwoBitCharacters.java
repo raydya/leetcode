07c572c9c3162f5cc4b11d5508def82f16e26ae8
zhanglei <dya.ilaempire@gmail.com>
2021-07-29 10:55:38 +0800

    is one bit character

---
+++ src/main/java/com/raydya/OneBitAndTwoBitCharacters.java
@@ -0,0 +1,18 @@
+package com.raydya;
+
+public class OneBitAndTwoBitCharacters {
+    public boolean isOneBitCharacter(int[] bits) {
+        int s = 0;
+        for (int i = 0; i < bits.length - 1; i++) {
+            if (i < s) continue;
+            final int bit = bits[i];
+            if (bit == 1) {
+                s = i + 2;
+            } else {
+                s = i + 1;
+            }
+        }
+
+        return s == bits.length - 1;
+    }
+}
---
+++ src/test/java/com/raydya/OneBitAndTwoBitCharactersTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OneBitAndTwoBitCharactersTest {
+    @Test
+    public void test1() {
+        final OneBitAndTwoBitCharacters solution = new OneBitAndTwoBitCharacters();
+        final boolean output = solution.isOneBitCharacter(new int[]{1, 0, 0});
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final OneBitAndTwoBitCharacters solution = new OneBitAndTwoBitCharacters();
+        final boolean output = solution.isOneBitCharacter(new int[]{1, 1, 1, 0});
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test3() {
+        final OneBitAndTwoBitCharacters solution = new OneBitAndTwoBitCharacters();
+        final boolean output = solution.isOneBitCharacter(new int[]{1, 1, 0});
+
+        Assert.assertTrue(output);
+    }
+}
