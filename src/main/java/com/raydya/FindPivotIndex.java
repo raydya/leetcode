de256aa865dd745d2ca4510071997e00cda4afac
zhanglei <dya.ilaempire@gmail.com>
2021-08-10 15:40:47 +0800

    find pivot index

---
+++ src/main/java/com/raydya/FindPivotIndex.java
@@ -0,0 +1,21 @@
+package com.raydya;
+
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        final int l = nums.length;
+        final int[] p = new int[l];
+        p[0] = nums[0];
+
+        for (int i = 1; i < l; i++) {
+            p[i] = p[i - 1] + nums[i];
+        }
+
+        if (p[l - 1] - p[0] == 0) return 0;
+
+        for (int i = 1; i < l; i++) {
+            if (p[i - 1] == (p[l - 1] - p[i])) return i;
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/java/com/raydya/FindPivotIndexTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindPivotIndexTest {
+    @Test
+    public void test1() {
+        final FindPivotIndex solution = new FindPivotIndex();
+        final int output = solution.pivotIndex(new int[]{1, 7, 3, 6, 5, 6});
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test2() {
+        final FindPivotIndex solution = new FindPivotIndex();
+        final int output = solution.pivotIndex(new int[]{1, 2, 3});
+
+        Assert.assertEquals(-1, output);
+    }
+
+    @Test
+    public void test3() {
+        final FindPivotIndex solution = new FindPivotIndex();
+        final int output = solution.pivotIndex(new int[]{2, 1, -1});
+
+        Assert.assertEquals(0, output);
+    }
+}
