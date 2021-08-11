673ad695e2512acd21d0bcdfd8fb041a3b629454
zhanglei <dya.ilaempire@gmail.com>
2021-08-11 11:59:01 +0800

    update

--- src/main/java/com/raydya/SelfDividingNumbers.java
+++ src/main/java/com/raydya/SelfDividingNumbers.java
@@ -7,20 +7,21 @@ public class SelfDividingNumbers {
     public List<Integer> selfDividingNumbers(int left, int right) {
         final List<Integer> ret = new ArrayList<>();
         for (int i = left; i <= right; i++) {
-            final char[] chars = String.valueOf(i).toCharArray();
             if (i < 10) {
                 ret.add(i);
-            } else {
-                boolean selfDividing = true;
-                for (final char aChar : chars) {
-                    if (aChar == '0' || i % (aChar - '0') != 0) {
-                        selfDividing = false;
-                        break;
-                    }
+                continue;
+            }
+            final char[] chars = String.valueOf(i).toCharArray();
+            boolean selfDividing = true;
+            for (int j = chars.length - 1; j >= 0; j--) {
+                final char aChar = chars[j];
+                if (aChar == '0' || i % (aChar - '0') != 0) {
+                    selfDividing = false;
+                    break;
                 }
-                if (selfDividing) {
-                    ret.add(i);
-                }
+            }
+            if (selfDividing) {
+                ret.add(i);
             }
         }
         return ret;
