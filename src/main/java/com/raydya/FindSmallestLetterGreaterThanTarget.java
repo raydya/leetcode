10db8d9c09aea55af3864cebf81a6bca1402822c
zhanglei <dya.ilaempire@gmail.com>
2021-08-18 11:18:23 +0800

    find smallest letter greater than target

---
+++ src/main/java/com/raydya/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,17 @@
+package com.raydya;
+
+public class FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        final int ia = target - 'a';
+        char ret = letters[0];
+        for (final char letter : letters) {
+            final int il = letter - 'a';
+            if (il > ia) {
+                ret = letter;
+                break;
+            }
+        }
+
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/FindSmallestLetterGreaterThanTargetTest.java
@@ -0,0 +1,54 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindSmallestLetterGreaterThanTargetTest {
+    @Test
+    public void test1() {
+        final FindSmallestLetterGreaterThanTarget solution = new FindSmallestLetterGreaterThanTarget();
+        final char output = solution.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'a');
+
+        Assert.assertEquals('c', output);
+    }
+
+    @Test
+    public void test2() {
+        final FindSmallestLetterGreaterThanTarget solution = new FindSmallestLetterGreaterThanTarget();
+        final char output = solution.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'c');
+
+        Assert.assertEquals('f', output);
+    }
+
+    @Test
+    public void test3() {
+        final FindSmallestLetterGreaterThanTarget solution = new FindSmallestLetterGreaterThanTarget();
+        final char output = solution.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'd');
+
+        Assert.assertEquals('f', output);
+    }
+
+    @Test
+    public void test4() {
+        final FindSmallestLetterGreaterThanTarget solution = new FindSmallestLetterGreaterThanTarget();
+        final char output = solution.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'g');
+
+        Assert.assertEquals('j', output);
+    }
+
+    @Test
+    public void test5() {
+        final FindSmallestLetterGreaterThanTarget solution = new FindSmallestLetterGreaterThanTarget();
+        final char output = solution.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'j');
+
+        Assert.assertEquals('c', output);
+    }
+
+    @Test
+    public void test6() {
+        final FindSmallestLetterGreaterThanTarget solution = new FindSmallestLetterGreaterThanTarget();
+        final char output = solution.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'k');
+
+        Assert.assertEquals('c', output);
+    }
+}
