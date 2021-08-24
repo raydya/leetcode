3873652923807034a07d79c00dc145ba6942037c
zhanglei <dya.ilaempire@gmail.com>
2021-08-24 17:44:07 +0800

    shortest completing word

---
+++ src/main/java/com/raydya/ShortestCompletingWord.java
@@ -0,0 +1,43 @@
+package com.raydya;
+
+public class ShortestCompletingWord {
+    private static final int ALPHABET_LENGTH = 26;
+
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        final char[] licensePlateCharArray = licensePlate.toLowerCase().toCharArray();
+        final int[] ts = new int[ALPHABET_LENGTH];
+
+        for (final char c1 : licensePlateCharArray) {
+            if (Character.isLetter(c1)) {
+                final int i = c1 - 'a';
+                ts[i] += 1;
+            }
+        }
+
+        String shortestCompletingWord = "";
+        int completingWordLength = Integer.MAX_VALUE;
+
+        for (final String word : words) {
+            if (word.length() < completingWordLength && isCompletingWord(ts, word.toCharArray())) {
+                shortestCompletingWord = word;
+                completingWordLength = word.length();
+            }
+        }
+
+        return shortestCompletingWord;
+    }
+
+    private boolean isCompletingWord(int[] ts, char[] chars) {
+        final int[] cs = new int[ALPHABET_LENGTH];
+        for (final char aChar : chars) {
+            final int i = aChar - 'a';
+            cs[i] += 1;
+        }
+
+        for (int i = 0; i < ALPHABET_LENGTH; i++) {
+            if (ts[i] > cs[i]) return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/raydya/ShortestCompletingWordTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShortestCompletingWordTest {
+    @Test
+    public void test1() {
+        final ShortestCompletingWord solution = new ShortestCompletingWord();
+        final String output = solution.shortestCompletingWord("1s3 PSt", new String[]{"step", "steps", "stripe", "stepple"});
+
+        Assert.assertEquals("steps", output);
+    }
+
+    @Test
+    public void test2() {
+        final ShortestCompletingWord solution = new ShortestCompletingWord();
+        final String output = solution.shortestCompletingWord("1s3 456", new String[]{"looks", "pest", "stew", "show"});
+
+        Assert.assertEquals("pest", output);
+    }
+
+    @Test
+    public void test3() {
+        final ShortestCompletingWord solution = new ShortestCompletingWord();
+        final String output = solution.shortestCompletingWord("Ah71752", new String[]{"suggest", "letter", "of", "husband", "easy", "education", "drug", "prevent", "writer", "old"});
+
+        Assert.assertEquals("husband", output);
+    }
+
+    @Test
+    public void test4() {
+        final ShortestCompletingWord solution = new ShortestCompletingWord();
+        final String output = solution.shortestCompletingWord("OgEu755", new String[]{"enough", "these", "play", "wide", "wonder", "box", "arrive", "money", "tax", "thus"});
+
+        Assert.assertEquals("enough", output);
+    }
+
+    @Test
+    public void test5() {
+        final ShortestCompletingWord solution = new ShortestCompletingWord();
+        final String output = solution.shortestCompletingWord("iMSlpe4", new String[]{"claim", "consumer", "student", "camera", "public", "never", "wonder", "simple", "thought", "use"});
+
+        Assert.assertEquals("simple", output);
+    }
+}
