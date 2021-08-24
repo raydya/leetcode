4aad5a3727a32dcc413cfbe6b867ecd07c0973e5
zhanglei <dya.ilaempire@gmail.com>
2021-08-24 14:52:12 +0800

    largest number at least twice of others

---
+++ src/main/java/com/raydya/LargestNumberAtLeastTwiceOfOthers.java
@@ -0,0 +1,25 @@
+package com.raydya;
+
+public class LargestNumberAtLeastTwiceOfOthers {
+    public int dominantIndex(int[] nums) {
+        if (nums.length == 1) return 0;
+        int max = 0;
+        int sMax = 0;
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            final int num = nums[i];
+            if (num > max) {
+                sMax = max;
+                max = num;
+                index = i;
+            } else if (num > sMax) {
+                sMax = num;
+            }
+        }
+
+        if (max >= sMax * 2) return index;
+
+        return -1;
+    }
+}
---
+++ src/test/java/com/raydya/LargestNumberAtLeastTwiceOfOthersTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LargestNumberAtLeastTwiceOfOthersTest {
+    @Test
+    public void test1() {
+        final LargestNumberAtLeastTwiceOfOthers solution = new LargestNumberAtLeastTwiceOfOthers();
+        final int output = solution.dominantIndex(new int[]{3, 6, 1, 0});
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test2() {
+        final LargestNumberAtLeastTwiceOfOthers solution = new LargestNumberAtLeastTwiceOfOthers();
+        final int output = solution.dominantIndex(new int[]{1, 2, 3, 4});
+
+        Assert.assertEquals(-1, output);
+    }
+
+    @Test
+    public void test3() {
+        final LargestNumberAtLeastTwiceOfOthers solution = new LargestNumberAtLeastTwiceOfOthers();
+        final int output = solution.dominantIndex(new int[]{1});
+
+        Assert.assertEquals(0, output);
+    }
+}
