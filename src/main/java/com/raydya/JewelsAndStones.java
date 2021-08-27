c75d59da2314d465c96044cdaf4350bc0a5fc5ae
zhanglei <dya.ilaempire@gmail.com>
2021-08-27 11:08:14 +0800

    jewels and stones

---
+++ src/main/java/com/raydya/JewelsAndStones.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+public class JewelsAndStones {
+    private static final int ALPHABET_LENGTH = 30;
+
+    public int numJewelsInStones(String jewels, String stones) {
+        final int[] alphabet = new int[ALPHABET_LENGTH * 2];
+        for (int i = 0; i < jewels.length(); i++) {
+            final char jewel = jewels.charAt(i);
+            final int k = jewel - 'A';
+            alphabet[k] = 1;
+        }
+        int cnt = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            final char stone = stones.charAt(i);
+            final int k = alphabet[stone - 'A'];
+            if (k == 1) cnt++;
+        }
+
+        return cnt;
+    }
+}
---
+++ src/test/java/com/raydya/JewelsAndStonesTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JewelsAndStonesTest {
+    @Test
+    public void test1() {
+        final JewelsAndStones solution = new JewelsAndStones();
+        final int output = solution.numJewelsInStones("aA", "aAAbbbb");
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test2() {
+        final JewelsAndStones solution = new JewelsAndStones();
+        final int output = solution.numJewelsInStones("z", "ZZ");
+
+        Assert.assertEquals(0, output);
+    }
+}
