7aaf77c2d0258047d969a55c359d93990d7f982e
zhanglei <dya.ilaempire@gmail.com>
2021-09-01 16:30:25 +0800

    unique morse code words

---
+++ src/main/java/com/raydya/UniqueMorseCodeWords.java
@@ -0,0 +1,29 @@
+package com.raydya;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseCodeWords {
+    private static final String[] MORSE_CODE_TABLE = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> uniqueCodes = new HashSet<>();
+        for (final String word : words) {
+            final String code = t(word);
+            uniqueCodes.add(code);
+        }
+
+        return uniqueCodes.size();
+    }
+
+    private String t(String word) {
+        String ret = "";
+        for (int i = 0; i < word.length(); i++) {
+            final char c = word.charAt(i);
+            final int k = c - 'a';
+            final String s = MORSE_CODE_TABLE[k];
+            ret += s;
+        }
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/UniqueMorseCodeWordsTest.java
@@ -0,0 +1,14 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UniqueMorseCodeWordsTest {
+    @Test
+    public void test1() {
+        final UniqueMorseCodeWords solution = new UniqueMorseCodeWords();
+        final int output = solution.uniqueMorseRepresentations(new String[]{"gin", "zen", "gig", "msg"});
+
+        Assert.assertEquals(2, output);
+    }
+}
