604a4fa856fc4fafd282148202b6ae0968cabea4
zhanglei <dya.ilaempire@gmail.com>
2021-09-06 12:08:43 +0800

    largest triangle area

---
+++ src/main/java/com/raydya/LargestTriangleArea.java
@@ -0,0 +1,20 @@
+package com.raydya;
+
+public class LargestTriangleArea {
+    public double largestTriangleArea(int[][] points) {
+        final int l = points.length;
+        double ans = 0;
+        for (int i = 0; i < l; ++i) {
+            for (int j = i + 1; j < l; ++j) {
+                for (int k = j + 1; k < l; ++k) {
+                    ans = Math.max(ans, area(points[i], points[j], points[k]));
+                }
+            }
+        }
+        return ans;
+    }
+
+    private double area(int[] p, int[] q, int[] r) {
+        return 0.5 * Math.abs(p[0] * q[1] + q[0] * r[1] + r[0] * p[1] - p[1] * q[0] - q[1] * r[0] - r[1] * p[0]);
+    }
+}
---
+++ src/test/java/com/raydya/LargestTriangleAreaTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LargestTriangleAreaTest {
+    @Test
+    public void test1() {
+        final LargestTriangleArea solution = new LargestTriangleArea();
+        final double output = solution.largestTriangleArea(new int[][]{{0, 0}, {0, 1}, {1, 0}, {0, 2}, {2, 0}});
+
+        Assert.assertEquals(2, output, 0);
+    }
+
+    @Test
+    public void test2() {
+        final LargestTriangleArea solution = new LargestTriangleArea();
+        final double output = solution.largestTriangleArea(new int[][]{{4, 6}, {6, 5}, {3, 1}});
+
+        Assert.assertEquals(5.5, output, 0);
+    }
+}
