980fb6808adceb750042599d4caaae419973d2c5
zhanglei <dya.ilaempire@gmail.com>
2021-09-08 10:49:18 +0800

    most common word

---
+++ src/main/java/com/raydya/MostCommonWord.java
@@ -0,0 +1,31 @@
+package com.raydya;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.toLowerCase();
+        final String[] words = paragraph.split("[!?',;.\\s]");
+
+        final Set<String> ban = new HashSet<>(Arrays.asList(banned));
+
+        Map<String, Integer> map = new HashMap<>();
+        String w = "";
+        int max = 0;
+        for (String word : words) {
+            if (word.isEmpty()) continue;
+            final int cnt = map.getOrDefault(word, 0) + 1;
+            map.put(word, cnt);
+            if (cnt > max && !ban.contains(word)) {
+                max = cnt;
+                w = word;
+            }
+        }
+
+        return w;
+    }
+}
---
+++ src/test/java/com/raydya/MostCommonWordTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MostCommonWordTest {
+    @Test
+    public void test1() {
+        final MostCommonWord solution = new MostCommonWord();
+        final String output = solution.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", new String[]{"hit"});
+
+        Assert.assertEquals("ball", output);
+    }
+
+    @Test
+    public void test2() {
+        final MostCommonWord solution = new MostCommonWord();
+        final String output = solution.mostCommonWord("a, a, a, a, b,b,b,c", new String[]{"a"});
+
+        Assert.assertEquals("b", output);
+    }
+}
