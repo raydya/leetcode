162064c27fa9df65d32faf450867e004c693ef71
zhanglei <dya.ilaempire@gmail.com>
2021-09-14 10:22:17 +0800

    goat latin

--- src/main/java/com/raydya/GoatLatin.java
+++ src/main/java/com/raydya/GoatLatin.java
@@ -1,8 +1,17 @@
 package com.raydya;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GoatLatin {
     public String toGoatLatin(String sentence) {
         final String[] strs = sentence.split(" ");
+
+        Map<Integer, String> ruleThree = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            ruleThree.put(i, ruleThree.getOrDefault(i - 1, "") + "a");
+        }
+
         for (int i = 0; i < strs.length; i++) {
             final String str = strs[i];
             if (isVowel(str)) {
@@ -11,11 +20,7 @@ public class GoatLatin {
                 strs[i] = str.substring(1) + str.charAt(0) + "ma";
             }
 
-            String iStr = "";
-            for (int j = 0; j < i + 1; j++) {
-                iStr += "a";
-            }
-            strs[i] += iStr;
+            strs[i] += ruleThree.get(i);
         }
 
         return String.join(" ", strs);
