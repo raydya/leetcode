09776b8f10f87d09199ad5d06bc09a89cca69a02
zhanglei <dya.ilaempire@gmail.com>
2021-10-09 10:14:03 +0800

    binary gap

---
+++ src/main/java/com/raydya/BinaryGap.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+public class BinaryGap {
+    public int binaryGap(int n) {
+        final String s = Integer.toBinaryString(n);
+
+        int gap = 0;
+        int p = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            if (c == '1') {
+                if (p != -1) {
+                    gap = Math.max(gap, i - p);
+                }
+                p = i;
+            }
+        }
+
+        return gap;
+    }
+}
---
+++ src/test/java/com/raydya/BinaryGapTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinaryGapTest {
+    @Test
+    public void test1() {
+        final BinaryGap solution = new BinaryGap();
+        final int output = solution.binaryGap(22);
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test2() {
+        final BinaryGap solution = new BinaryGap();
+        final int output = solution.binaryGap(5);
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test3() {
+        final BinaryGap solution = new BinaryGap();
+        final int output = solution.binaryGap(6);
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test4() {
+        final BinaryGap solution = new BinaryGap();
+        final int output = solution.binaryGap(8);
+
+        Assert.assertEquals(0, output);
+    }
+
+    @Test
+    public void test5() {
+        final BinaryGap solution = new BinaryGap();
+        final int output = solution.binaryGap(1);
+
+        Assert.assertEquals(0, output);
+    }
+}
