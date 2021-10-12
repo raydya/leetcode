b52aa538f51d55b60f4145f3837d3702b7cca5a8
zhanglei <dya.ilaempire@gmail.com>
2021-10-12 10:43:16 +0800

    middle of the linked list

---
+++ src/main/java/com/raydya/MiddleOfTheLinkedList.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import com.raydya.data.type.ListNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MiddleOfTheLinkedList {
+    public ListNode middleNode(ListNode head) {
+        List<ListNode> nodes = new ArrayList<>();
+
+        ListNode node = head;
+
+        while (node != null) {
+            nodes.add(node);
+            node = node.next;
+        }
+
+        final int i = nodes.size() / 2;
+        return nodes.get(i);
+    }
+}
---
+++ src/test/java/com/raydya/MiddleOfTheLinkedListTest.java
@@ -0,0 +1,28 @@
+package com.raydya;
+
+import com.raydya.data.type.ListNode;
+import com.raydya.utils.ListNodeUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MiddleOfTheLinkedListTest {
+    @Test
+    public void test1() {
+        final ListNode head = ListNodeUtil.build(new int[]{1, 2, 3, 4, 5});
+
+        final MiddleOfTheLinkedList solution = new MiddleOfTheLinkedList();
+        final ListNode output = solution.middleNode(head);
+
+        Assert.assertEquals(head.next.next, output);
+    }
+
+    @Test
+    public void test2() {
+        final ListNode head = ListNodeUtil.build(new int[]{1, 2, 3, 4, 5, 6});
+
+        final MiddleOfTheLinkedList solution = new MiddleOfTheLinkedList();
+        final ListNode output = solution.middleNode(head);
+
+        Assert.assertEquals(head.next.next.next, output);
+    }
+}
