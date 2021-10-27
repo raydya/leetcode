ef4b98aa7c2c99fc6d30c1bdf2dfd72627a18128
zhanglei <dya.ilaempire@gmail.com>
2021-10-27 09:55:47 +0800

    x of a kind in a deck of cards

---
+++ src/main/java/com/raydya/XOfAKindInADeckOfCards.java
@@ -0,0 +1,27 @@
+package com.raydya;
+
+public class XOfAKindInADeckOfCards {
+    public boolean hasGroupsSizeX(int[] deck) {
+        int[] count = new int[10000];
+        for (final int i : deck) {
+            count[i]++;
+        }
+
+        int g = -1;
+        for (final int j : count) {
+            if (j > 0) {
+                if (g == -1) {
+                    g = j;
+                } else {
+                    g = gcd(g, j);
+                }
+            }
+        }
+
+        return g >= 2;
+    }
+
+    public int gcd(int x, int y) {
+        return x == 0 ? y : gcd(y % x, x);
+    }
+}
---
+++ src/test/java/com/raydya/XOfAKindInADeckOfCardsTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class XOfAKindInADeckOfCardsTest {
+    @Test
+    public void test1() {
+        final XOfAKindInADeckOfCards solution = new XOfAKindInADeckOfCards();
+        final boolean output = solution.hasGroupsSizeX(new int[]{1, 2, 3, 4, 4, 3, 2, 1});
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final XOfAKindInADeckOfCards solution = new XOfAKindInADeckOfCards();
+        final boolean output = solution.hasGroupsSizeX(new int[]{1, 1, 1, 2, 2, 2, 3, 3});
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test3() {
+        final XOfAKindInADeckOfCards solution = new XOfAKindInADeckOfCards();
+        final boolean output = solution.hasGroupsSizeX(new int[]{1});
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test4() {
+        final XOfAKindInADeckOfCards solution = new XOfAKindInADeckOfCards();
+        final boolean output = solution.hasGroupsSizeX(new int[]{1, 1});
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test5() {
+        final XOfAKindInADeckOfCards solution = new XOfAKindInADeckOfCards();
+        final boolean output = solution.hasGroupsSizeX(new int[]{1, 1, 2, 2, 2, 2});
+
+        Assert.assertTrue(output);
+    }
+}
