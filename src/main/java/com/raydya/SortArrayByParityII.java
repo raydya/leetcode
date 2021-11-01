b45fb1bcfc9edff1debbb80b2cbd8416d80723b0
zhanglei <dya.ilaempire@gmail.com>
2021-11-01 10:28:14 +0800

    sort array by parity ii

---
+++ src/main/java/com/raydya/SortArrayByParityII.java
@@ -0,0 +1,21 @@
+package com.raydya;
+
+public class SortArrayByParityII {
+    public int[] sortArrayByParityII(int[] nums) {
+        final int[] ret = new int[nums.length];
+
+        int even = 0;
+        int odd = 1;
+        for (final int n : nums) {
+            if (n % 2 == 0) {
+                ret[even] = n;
+                even += 2;
+            } else {
+                ret[odd] = n;
+                odd += 2;
+            }
+        }
+
+        return ret;
+    }
+}
