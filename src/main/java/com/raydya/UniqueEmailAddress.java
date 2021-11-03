f8ffbc08d1b903b9c420f8c0442165a2c7c005eb
zhanglei <dya.ilaempire@gmail.com>
2021-11-03 10:49:42 +0800

    unique email address

---
+++ src/main/java/com/raydya/UniqueEmailAddress.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueEmailAddress {
+    public int numUniqueEmails(String[] emails) {
+        final Set<String> uniqueEmails = new HashSet<>();
+        for (final String email : emails) {
+            final String s = formatEmail(email);
+            uniqueEmails.add(s);
+        }
+
+        return uniqueEmails.size();
+    }
+
+    private String formatEmail(String email) {
+        StringBuilder ret = new StringBuilder();
+
+        boolean afterPlus = false;
+
+        int k = 0;
+        for (int i = 0; i < email.length(); i++) {
+            final char c = email.charAt(i);
+            if (c == '@') {
+                k = i;
+                break;
+            }
+
+            if (c == '.') {
+                continue;
+            }
+
+            if (c == '+' || afterPlus) {
+                afterPlus = true;
+                continue;
+            }
+
+            ret.append(c);
+        }
+
+        ret.append(email.substring(k));
+
+        return ret.toString();
+    }
+}
---
+++ src/test/java/com/raydya/UniqueEmailAddressTest.java
@@ -0,0 +1,34 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UniqueEmailAddressTest {
+    @Test
+    public void test1() {
+        final UniqueEmailAddress solution = new UniqueEmailAddress();
+        final int output = solution.numUniqueEmails(
+            new String[]{
+                "test.email+alex@leetcode.com",
+                "test.e.mail+bob.cathy@leetcode.com",
+                "testemail+david@lee.tcode.com"
+            }
+        );
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test2() {
+        final UniqueEmailAddress solution = new UniqueEmailAddress();
+        final int output = solution.numUniqueEmails(
+            new String[]{
+                "a@leetcode.com",
+                "b@leetcode.com",
+                "c@leetcode.com"
+            }
+        );
+
+        Assert.assertEquals(3, output);
+    }
+}
