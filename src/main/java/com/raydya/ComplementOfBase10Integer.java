d6ad1f255af202d3beb696e80026a0d203d8ac0a
zhanglei <dya.ilaempire@gmail.com>
2021-11-25 09:58:58 +0800

    complement of base 10 integer

---
+++ src/main/java/com/raydya/ComplementOfBase10Integer.java
@@ -0,0 +1,12 @@
+package com.raydya;
+
+public class ComplementOfBase10Integer {
+    public int bitwiseComplement(int n) {
+        int _n = n, mask = 1;
+        while (_n != 0) {
+            mask = _n & (~_n + 1);
+            _n ^= mask;
+        }
+        return ~n & ((mask << 1) - 1);
+    }
+}
---
+++ src/test/java/com/raydya/ComplementOfBase10IntegerTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ComplementOfBase10IntegerTest {
+    @Test
+    public void test1() {
+        final ComplementOfBase10Integer solution = new ComplementOfBase10Integer();
+        final int output = solution.bitwiseComplement(5);
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test2() {
+        final ComplementOfBase10Integer solution = new ComplementOfBase10Integer();
+        final int output = solution.bitwiseComplement(7);
+
+        Assert.assertEquals(0, output);
+    }
+
+    @Test
+    public void test3() {
+        final ComplementOfBase10Integer solution = new ComplementOfBase10Integer();
+        final int output = solution.bitwiseComplement(10);
+
+        Assert.assertEquals(5, output);
+    }
+}
