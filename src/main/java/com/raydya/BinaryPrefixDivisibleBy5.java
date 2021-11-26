4a410b7afed792f72fa0ce8deb88a4e4860c3e30
zhanglei <dya.ilaempire@gmail.com>
2021-11-26 11:45:38 +0800

    binary prefix divisible by 5

---
+++ src/main/java/com/raydya/BinaryPrefixDivisibleBy5.java
@@ -0,0 +1,19 @@
+package com.raydya;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryPrefixDivisibleBy5 {
+    public List<Boolean> prefixesDivBy5(int[] nums) {
+        List<Boolean> output = new ArrayList<>();
+
+        int p = 0;
+        for (final int num : nums) {
+            p = 2 * p + num;
+            output.add(p % 5 == 0);
+            p = p % 5;
+        }
+
+        return output;
+    }
+}
---
+++ src/test/java/com/raydya/BinaryPrefixDivisibleBy5Test.java
@@ -0,0 +1,61 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class BinaryPrefixDivisibleBy5Test {
+    @Test
+    public void test1() {
+        final BinaryPrefixDivisibleBy5 solution = new BinaryPrefixDivisibleBy5();
+        final List<Boolean> output = solution.prefixesDivBy5(new int[]{0, 1, 1});
+
+        Assert.assertArrayEquals(new Boolean[]{true, false, false}, output.toArray());
+    }
+
+    @Test
+    public void test2() {
+        final BinaryPrefixDivisibleBy5 solution = new BinaryPrefixDivisibleBy5();
+        final List<Boolean> output = solution.prefixesDivBy5(new int[]{1, 1, 1});
+
+        Assert.assertArrayEquals(new Boolean[]{false, false, false}, output.toArray());
+    }
+
+    @Test
+    public void test3() {
+        final BinaryPrefixDivisibleBy5 solution = new BinaryPrefixDivisibleBy5();
+        final List<Boolean> output = solution.prefixesDivBy5(new int[]{0, 1, 1, 1, 1, 1});
+
+        Assert.assertArrayEquals(new Boolean[]{true, false, false, false, true, false}, output.toArray());
+    }
+
+    @Test
+    public void test4() {
+        final BinaryPrefixDivisibleBy5 solution = new BinaryPrefixDivisibleBy5();
+        final List<Boolean> output = solution.prefixesDivBy5(new int[]{1, 1, 1, 0, 1});
+
+        Assert.assertArrayEquals(new Boolean[]{false, false, false, false, false}, output.toArray());
+    }
+
+    @Test
+    public void test5() {
+        final BinaryPrefixDivisibleBy5 solution = new BinaryPrefixDivisibleBy5();
+        final List<Boolean> output = solution.prefixesDivBy5(
+            new int[]{
+                1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1
+            }
+        );
+
+        Assert.assertArrayEquals(
+            new Boolean[]{
+                false, false, false, false, false, false, false, false, false, false, false, false,
+                false, false, false, false, false, false, false, false, false, false, false, false,
+                false, false, false, false, false, false, false, true, false, false, true, true, true,
+                true, false
+            },
+            output.toArray()
+        );
+    }
+}
