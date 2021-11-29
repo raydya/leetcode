2d2574b771d34c691c0e8f7a1b3df28c77c00b19
zhanglei <dya.ilaempire@gmail.com>
2021-11-29 11:10:46 +0800

    remove outermost parentheses

---
+++ src/test/java/com/raydya/RemoveOutermostParenthesesTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemoveOutermostParenthesesTest {
+    @Test
+    public void test1() {
+        final RemoveOutermostParentheses solution = new RemoveOutermostParentheses();
+        final String output = solution.removeOuterParentheses("(()())(())");
+
+        Assert.assertEquals("()()()", output);
+    }
+
+    @Test
+    public void test2() {
+        final RemoveOutermostParentheses solution = new RemoveOutermostParentheses();
+        final String output = solution.removeOuterParentheses("(()())(())(()(()))");
+
+        Assert.assertEquals("()()()()(())", output);
+    }
+
+    @Test
+    public void test3() {
+        final RemoveOutermostParentheses solution = new RemoveOutermostParentheses();
+        final String output = solution.removeOuterParentheses("()()");
+
+        Assert.assertEquals("", output);
+    }
+}
