ea4a5155b3be6c7b7ac945a6a6ec86ae73d340ce
ZhangLei <dya.ilaempire@gmail.com>
2021-12-06 10:15:28 +0800

    truncate sentence

---
+++ src/main/java/com/raydya/TruncateSentence.java
@@ -0,0 +1,20 @@
+package com.raydya;
+
+public class TruncateSentence {
+    public String truncateSentence(String s, int k) {
+        final StringBuilder ret = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            if (c == ' ') {
+                k--;
+            }
+            if (k == 0) {
+                break;
+            }
+            ret.append(c);
+        }
+
+        return ret.toString();
+    }
+}
---
+++ src/test/java/com/raydya/TruncateSentenceTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TruncateSentenceTest {
+    @Test
+    public void test1() {
+        final TruncateSentence solution = new TruncateSentence();
+        final String output = solution.truncateSentence("Hello how are you Contestant", 4);
+
+        Assert.assertEquals("Hello how are you", output);
+    }
+
+    @Test
+    public void test2() {
+        final TruncateSentence solution = new TruncateSentence();
+        final String output = solution.truncateSentence("What is the solution to this problem", 4);
+
+        Assert.assertEquals("What is the solution", output);
+    }
+
+    @Test
+    public void test3() {
+        final TruncateSentence solution = new TruncateSentence();
+        final String output = solution.truncateSentence("chopper is not a tanuki", 5);
+
+        Assert.assertEquals("chopper is not a tanuki", output);
+    }
+}
