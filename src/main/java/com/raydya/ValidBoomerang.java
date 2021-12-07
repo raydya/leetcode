7c079a61f35ddfc509887cd72d1e706d26267c4f
ZhangLei <dya.ilaempire@gmail.com>
2021-12-07 11:39:52 +0800

    valid boomerang

---
+++ src/main/java/com/raydya/ValidBoomerang.java
@@ -0,0 +1,19 @@
+package com.raydya;
+
+public class ValidBoomerang {
+    public boolean isBoomerang(int[][] points) {
+        final int[] a = points[0];
+        final int[] b = points[1];
+        final int[] c = points[2];
+        final int x1 = a[0];
+        final int y1 = a[1];
+        final int x2 = b[0];
+        final int y2 = b[1];
+        final int x3 = c[0];
+        final int y3 = c[1];
+
+        double s = (x1 * y2 - x2 * y1 + x2 * y3 - x3 * y2 + x3 * y1 - x1 * y3) / 2.0;
+
+        return s != 0.0;
+    }
+}
---
+++ src/test/java/com/raydya/ValidBoomerangTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ValidBoomerangTest {
+    @Test
+    public void test1() {
+        final ValidBoomerang solution = new ValidBoomerang();
+        final boolean output = solution.isBoomerang(new int[][]{{1, 1}, {2, 3}, {3, 2}});
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final ValidBoomerang solution = new ValidBoomerang();
+        final boolean output = solution.isBoomerang(new int[][]{{1, 1}, {2, 2}, {3, 3}});
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test3() {
+        final ValidBoomerang solution = new ValidBoomerang();
+        final boolean output = solution.isBoomerang(new int[][]{{0, 0}, {1, 2}, {0, 1}});
+
+        Assert.assertTrue(output);
+    }
+}
