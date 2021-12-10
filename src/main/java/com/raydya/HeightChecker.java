a3a938fb04e345cbd0d285a8621328d20187bb08
ZhangLei <dya.ilaempire@gmail.com>
2021-12-10 09:44:30 +0800

    height checker

---
+++ src/main/java/com/raydya/HeightChecker.java
@@ -0,0 +1,17 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class HeightChecker {
+    public int heightChecker(int[] heights) {
+        final int[] expected = heights.clone();
+        Arrays.sort(expected);
+
+        int cnt = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i]) cnt++;
+        }
+
+        return cnt;
+    }
+}
---
+++ src/test/java/com/raydya/HeightCheckerTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HeightCheckerTest {
+    @Test
+    public void test1() {
+        final HeightChecker solution = new HeightChecker();
+        final int output = solution.heightChecker(new int[]{1, 1, 4, 2, 1, 3});
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test2() {
+        final HeightChecker solution = new HeightChecker();
+        final int output = solution.heightChecker(new int[]{5, 1, 2, 3, 4});
+
+        Assert.assertEquals(5, output);
+    }
+
+    @Test
+    public void test3() {
+        final HeightChecker solution = new HeightChecker();
+        final int output = solution.heightChecker(new int[]{1, 2, 3, 4, 5});
+
+        Assert.assertEquals(0, output);
+    }
+}
