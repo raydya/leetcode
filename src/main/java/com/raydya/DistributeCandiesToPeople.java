691435d713221333f9944e12c7e8ddabcc139d99
ZhangLei <dya.ilaempire@gmail.com>
2021-12-22 09:56:55 +0800

    distribute candies to people

---
+++ src/main/java/com/raydya/DistributeCandiesToPeople.java
@@ -0,0 +1,23 @@
+package com.raydya;
+
+public class DistributeCandiesToPeople {
+    public int[] distributeCandies(int candies, int num_people) {
+        final int[] ret = new int[num_people];
+
+        int cnt = 1;
+        int i = 0;
+
+        while (candies > 0) {
+            final int k = Math.min(cnt, candies);
+            ret[i] += k;
+            candies -= k;
+            cnt++;
+            i++;
+            if (i > num_people - 1) {
+                i = 0;
+            }
+        }
+
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/DistributeCandiesToPeopleTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DistributeCandiesToPeopleTest {
+    @Test
+    public void test1() {
+        final DistributeCandiesToPeople solution = new DistributeCandiesToPeople();
+        final int[] output = solution.distributeCandies(7, 4);
+
+        Assert.assertArrayEquals(new int[]{1, 2, 3, 1}, output);
+    }
+
+    @Test
+    public void test2() {
+        final DistributeCandiesToPeople solution = new DistributeCandiesToPeople();
+        final int[] output = solution.distributeCandies(10, 3);
+
+        Assert.assertArrayEquals(new int[]{5, 2, 3}, output);
+    }
+}
