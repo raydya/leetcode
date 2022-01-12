cd227b2a7dedeb1beef5dfb97852269b71db734a
ZhangLei <dya.ilaempire@gmail.com>
2022-01-12 19:07:22 +0800

    maximum-number-of-balloons

---
+++ src/main/java/com/raydya/MaximumNumberOfBalloons.java
@@ -0,0 +1,37 @@
+package com.raydya;
+
+public class MaximumNumberOfBalloons {
+    public int maxNumberOfBalloons(String text) {
+        int[] alphabets = new int[26];
+
+        for (int i = 0; i < text.length(); i++) {
+            final char c = text.charAt(i);
+            alphabets[c - 'a']++;
+        }
+
+        int ans = 0;
+        final int b = 'b' - 'a';
+        final int a = 0;
+        final int l = 'l' - 'a';
+        final int o = 'o' - 'a';
+        final int n = 'n' - 'a';
+        while (true) {
+            alphabets[b]--;
+            alphabets[a]--;
+            alphabets[l] -= 2;
+            alphabets[o] -= 2;
+            alphabets[n]--;
+            if (alphabets[b] >= 0
+                && alphabets[a] >= 0
+                && alphabets[l] >= 0
+                && alphabets[o] >= 0
+                && alphabets[n] >= 0) {
+                ans++;
+            } else {
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ src/test/java/com/raydya/MaximumNumberOfBalloonsTest.java
@@ -0,0 +1,38 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaximumNumberOfBalloonsTest {
+    @Test
+    public void test1() {
+        final MaximumNumberOfBalloons solution = new MaximumNumberOfBalloons();
+        final int output = solution.maxNumberOfBalloons("nlaebolko");
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test2() {
+        final MaximumNumberOfBalloons solution = new MaximumNumberOfBalloons();
+        final int output = solution.maxNumberOfBalloons("loonbalxballpoon");
+
+        Assert.assertEquals(2, output);
+    }
+
+    @Test
+    public void test3() {
+        final MaximumNumberOfBalloons solution = new MaximumNumberOfBalloons();
+        final int output = solution.maxNumberOfBalloons("leetcode");
+
+        Assert.assertEquals(0, output);
+    }
+
+    @Test
+    public void test4() {
+        final MaximumNumberOfBalloons solution = new MaximumNumberOfBalloons();
+        final int output = solution.maxNumberOfBalloons("balon");
+
+        Assert.assertEquals(0, output);
+    }
+}
