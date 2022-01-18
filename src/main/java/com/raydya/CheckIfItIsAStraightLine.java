4441d2c4ff8a13bad7ff001398720cfd1de003d0
ZhangLei <dya.ilaempire@gmail.com>
2022-01-18 09:59:55 +0800

    check-if-it-is-a-straight-line

---
+++ src/main/java/com/raydya/CheckIfItIsAStraightLine.java
@@ -0,0 +1,17 @@
+package com.raydya;
+
+public class CheckIfItIsAStraightLine {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if (coordinates.length == 2) return true;
+        for (int i = 0; i < coordinates.length - 2; i++) {
+            if (!isLine(coordinates[i], coordinates[i + 1], coordinates[i + 2])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isLine(int[] p1, int[] p2, int[] p3) {
+        return (p2[1] - p1[1]) * (p3[0] - p1[0]) == (p3[1] - p1[1]) * (p2[0] - p1[0]);
+    }
+}
---
+++ src/test/java/com/raydya/CheckIfItIsAStraightLineTest.java
@@ -0,0 +1,26 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckIfItIsAStraightLineTest {
+    @Test
+    public void test1() {
+        final int[][] coordinates = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}};
+
+        final CheckIfItIsAStraightLine solution = new CheckIfItIsAStraightLine();
+        final boolean output = solution.checkStraightLine(coordinates);
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final int[][] coordinates = {{1, 1}, {2, 2}, {3, 4}, {4, 5}, {5, 6}, {7, 7}};
+
+        final CheckIfItIsAStraightLine solution = new CheckIfItIsAStraightLine();
+        final boolean output = solution.checkStraightLine(coordinates);
+
+        Assert.assertFalse(output);
+    }
+}
