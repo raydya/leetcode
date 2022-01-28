62d014227056fb683223015506bdc5f4f7e78c4b
ZhangLei <dya.ilaempire@gmail.com>
2022-01-28 11:23:43 +0800

    replace-elements-with-greatest-element-on-right-side

---
+++ src/main/java/com/raydya/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,14 @@
+package com.raydya;
+
+public class ReplaceElementsWithGreatestElementOnRightSide {
+    public int[] replaceElements(int[] arr) {
+        int max = -1;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            final int k = arr[i];
+            arr[i] = max;
+            max = Math.max(k, max);
+        }
+
+        return arr;
+    }
+}
---
+++ src/test/java/com/raydya/ReplaceElementsWithGreatestElementOnRightSideTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReplaceElementsWithGreatestElementOnRightSideTest {
+    @Test
+    public void test1() {
+        final ReplaceElementsWithGreatestElementOnRightSide solution = new ReplaceElementsWithGreatestElementOnRightSide();
+        final int[] output = solution.replaceElements(new int[]{17, 18, 5, 4, 6, 1});
+
+        Assert.assertArrayEquals(new int[]{18, 6, 6, 6, 1, -1}, output);
+    }
+
+    @Test
+    public void test2() {
+        final ReplaceElementsWithGreatestElementOnRightSide solution = new ReplaceElementsWithGreatestElementOnRightSide();
+        final int[] output = solution.replaceElements(new int[]{400});
+
+        Assert.assertArrayEquals(new int[]{-1}, output);
+    }
+}
