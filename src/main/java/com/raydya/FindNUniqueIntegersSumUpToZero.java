4768df9517a0d821c0d612da5c4c1cb2ef91c354
ZhangLei <dya.ilaempire@gmail.com>
2022-01-29 09:58:39 +0800

    find-n-unique-integers-sum-up-to-zero

---
+++ src/main/java/com/raydya/FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,15 @@
+package com.raydya;
+
+public class FindNUniqueIntegersSumUpToZero {
+    public int[] sumZero(int n) {
+        final int[] ret = new int[n];
+        int s = n / 2;
+        final int length = ret.length;
+        for (int i = 0; i < length / 2; i++) {
+            ret[i] = -s;
+            ret[length - 1 - i] = s;
+            s--;
+        }
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/FindNUniqueIntegersSumUpToZeroTest.java
@@ -0,0 +1,38 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindNUniqueIntegersSumUpToZeroTest {
+    @Test
+    public void test1() {
+        final FindNUniqueIntegersSumUpToZero solution = new FindNUniqueIntegersSumUpToZero();
+        final int[] output = solution.sumZero(5);
+
+        Assert.assertArrayEquals(new int[]{-2, -1, 0, 1, 2}, output);
+    }
+
+    @Test
+    public void test2() {
+        final FindNUniqueIntegersSumUpToZero solution = new FindNUniqueIntegersSumUpToZero();
+        final int[] output = solution.sumZero(3);
+
+        Assert.assertArrayEquals(new int[]{-1, 0, 1}, output);
+    }
+
+    @Test
+    public void test3() {
+        final FindNUniqueIntegersSumUpToZero solution = new FindNUniqueIntegersSumUpToZero();
+        final int[] output = solution.sumZero(1);
+
+        Assert.assertArrayEquals(new int[]{0}, output);
+    }
+
+    @Test
+    public void test4() {
+        final FindNUniqueIntegersSumUpToZero solution = new FindNUniqueIntegersSumUpToZero();
+        final int[] output = solution.sumZero(2);
+
+        Assert.assertArrayEquals(new int[]{-1, 1}, output);
+    }
+}
