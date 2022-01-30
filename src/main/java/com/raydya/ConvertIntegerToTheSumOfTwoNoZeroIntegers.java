f19c8d3f19ee1185887628b20aada9357d676ea2
ZhangLei <dya.ilaempire@gmail.com>
2022-01-30 09:34:43 +0800

    convert-integet-to-the-sum-of-two-no-zero-integers

---
+++ src/main/java/com/raydya/ConvertIntegerToTheSumOfTwoNoZeroIntegers.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+public class ConvertIntegerToTheSumOfTwoNoZeroIntegers {
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i < n; i++) {
+            final int j = n - i;
+            if (isNoZeroInteger(i) && isNoZeroInteger(j)) {
+                return new int[]{i, j};
+            }
+        }
+        return new int[]{};
+    }
+
+    private boolean isNoZeroInteger(int i) {
+        final String s = String.valueOf(i);
+        for (int j = 0; j < s.length(); j++) {
+            final char c = s.charAt(j);
+            if (c == '0') return false;
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/raydya/ConvertIntegerToTheSumOfTwoNoZeroIntegersTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConvertIntegerToTheSumOfTwoNoZeroIntegersTest {
+    @Test
+    public void test1() {
+        final ConvertIntegerToTheSumOfTwoNoZeroIntegers solution = new ConvertIntegerToTheSumOfTwoNoZeroIntegers();
+        final int[] output = solution.getNoZeroIntegers(2);
+
+        Assert.assertArrayEquals(new int[]{1, 1}, output);
+    }
+
+    @Test
+    public void test2() {
+        final ConvertIntegerToTheSumOfTwoNoZeroIntegers solution = new ConvertIntegerToTheSumOfTwoNoZeroIntegers();
+        final int[] output = solution.getNoZeroIntegers(11);
+
+        Assert.assertArrayEquals(new int[]{2, 9}, output);
+    }
+
+    @Test
+    public void test3() {
+        final ConvertIntegerToTheSumOfTwoNoZeroIntegers solution = new ConvertIntegerToTheSumOfTwoNoZeroIntegers();
+        final int[] output = solution.getNoZeroIntegers(10000);
+
+        Assert.assertArrayEquals(new int[]{1, 9999}, output);
+    }
+
+    @Test
+    public void test4() {
+        final ConvertIntegerToTheSumOfTwoNoZeroIntegers solution = new ConvertIntegerToTheSumOfTwoNoZeroIntegers();
+        final int[] output = solution.getNoZeroIntegers(69);
+
+        Assert.assertArrayEquals(new int[]{1, 68}, output);
+    }
+
+    @Test
+    public void test5() {
+        final ConvertIntegerToTheSumOfTwoNoZeroIntegers solution = new ConvertIntegerToTheSumOfTwoNoZeroIntegers();
+        final int[] output = solution.getNoZeroIntegers(1010);
+
+        Assert.assertArrayEquals(new int[]{11, 999}, output);
+    }
+}
