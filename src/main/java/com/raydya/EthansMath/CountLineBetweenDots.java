cb2972fe432627864e019a3d07063a3dd1b2a4d3
ZhangLei <dya.ilaempire@gmail.com>
2022-02-01 11:45:43 +0800

    count-line-between-dots

---
+++ src/main/java/com/raydya/EthansMath/CountLineBetweenDots.java
@@ -0,0 +1,14 @@
+package com.raydya.EthansMath;
+
+public class CountLineBetweenDots {
+    public int countLine(int dots) {
+        int step = 0;
+        int t = 0;
+        while (dots > 0) {
+            t += step;
+            step++;
+            dots--;
+        }
+        return t;
+    }
+}
---
+++ src/test/java/com/raydya/CountLineBetweenDotsTest.java
@@ -0,0 +1,63 @@
+package com.raydya;
+
+import com.raydya.EthansMath.CountLineBetweenDots;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CountLineBetweenDotsTest {
+    @Test
+    public void test1() {
+        final CountLineBetweenDots solution = new CountLineBetweenDots();
+        final int output = solution.countLine(2);
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test2() {
+        final CountLineBetweenDots solution = new CountLineBetweenDots();
+        final int output = solution.countLine(3);
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test3() {
+        final CountLineBetweenDots solution = new CountLineBetweenDots();
+        final int output = solution.countLine(4);
+
+        Assert.assertEquals(6, output);
+    }
+
+    @Test
+    public void test4() {
+        final CountLineBetweenDots solution = new CountLineBetweenDots();
+        final int output = solution.countLine(5);
+
+        Assert.assertEquals(10, output);
+    }
+
+    @Test
+    public void test5() {
+        final CountLineBetweenDots solution = new CountLineBetweenDots();
+        final int output = solution.countLine(6);
+
+        Assert.assertEquals(15, output);
+    }
+
+    @Test
+    public void test6() {
+        final CountLineBetweenDots solution = new CountLineBetweenDots();
+        final int output = solution.countLine(1000);
+
+        Assert.assertEquals(499500, output);
+    }
+
+    @Test
+    public void test7() {
+        final CountLineBetweenDots solution = new CountLineBetweenDots();
+        final int output = solution.countLine(7);
+
+        Assert.assertEquals(21, output);
+    }
+}
