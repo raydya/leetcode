4fdfb62fa28527187169d598ccf2bb128a6a3c78
ZhangLei <dya.ilaempire@gmail.com>
2022-02-07 10:26:01 +0800

    rank-transform-of-an-array

---
+++ src/main/java/com/raydya/RankTransformOfAnArray.java
@@ -0,0 +1,32 @@
+package com.raydya;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RankTransformOfAnArray {
+    public int[] arrayRankTransform(int[] arr) {
+        final int[] t = arr.clone();
+        Arrays.sort(t);
+
+        final Map<Integer, Integer> rankMap = new HashMap<>();
+        int c = 0;
+        for (int i = 0; i < t.length; i++) {
+            final int k = t[i];
+            final Integer j = rankMap.getOrDefault(k, 0);
+            if (j != 0) {
+                c++;
+            }
+            rankMap.put(k, i - c + 1);
+        }
+
+        final int[] ret = new int[arr.length];
+
+        for (int i = 0; i < ret.length; i++) {
+            final Integer j = rankMap.get(arr[i]);
+            ret[i] = j;
+        }
+
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/RankTransformOfAnArrayTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RankTransformOfAnArrayTest {
+    @Test
+    public void test1() {
+        final RankTransformOfAnArray solution = new RankTransformOfAnArray();
+        final int[] output = solution.arrayRankTransform(new int[]{40, 10, 20, 30});
+
+        Assert.assertArrayEquals(new int[]{4, 1, 2, 3}, output);
+    }
+
+    @Test
+    public void test2() {
+        final RankTransformOfAnArray solution = new RankTransformOfAnArray();
+        final int[] output = solution.arrayRankTransform(new int[]{100, 100, 100});
+
+        Assert.assertArrayEquals(new int[]{1, 1, 1}, output);
+    }
+
+    @Test
+    public void test3() {
+        final RankTransformOfAnArray solution = new RankTransformOfAnArray();
+        final int[] output = solution.arrayRankTransform(new int[]{37, 12, 28, 9, 100, 56, 80, 5, 12});
+
+        Assert.assertArrayEquals(new int[]{5, 3, 4, 2, 8, 6, 7, 1, 3}, output);
+    }
+}
