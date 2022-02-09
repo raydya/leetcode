46ce3086e0cb4ef165edce1f45db52f3b553eb71
ZhangLei <dya.ilaempire@gmail.com>
2022-02-09 20:05:33 +0800

    sort-integers-by-the-number-of-bits-update-1

--- src/main/java/com/raydya/SortIntegersByTheNumberOf1Bits.java
+++ src/main/java/com/raydya/SortIntegersByTheNumberOf1Bits.java
@@ -1,11 +1,26 @@
 package com.raydya;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SortIntegersByTheNumberOf1Bits {
     public int[] sortByBits(int[] arr) {
-        return Arrays.stream(arr).boxed().sorted(Comparator.comparingInt(Integer::bitCount).thenComparingInt(a -> a))
-                     .mapToInt(Integer::intValue).toArray();
+        int[] bit = new int[10001];
+        List<Integer> list = new ArrayList<>();
+        for (int x : arr) {
+            list.add(x);
+            bit[x] = Integer.bitCount(x);
+        }
+        list.sort((x, y) -> {
+            if (bit[x] != bit[y]) {
+                return bit[x] - bit[y];
+            } else {
+                return x - y;
+            }
+        });
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = list.get(i);
+        }
+        return arr;
     }
 }
