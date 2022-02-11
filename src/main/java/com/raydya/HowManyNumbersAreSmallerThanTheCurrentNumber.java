34944e735ab30c111b9471e33b4808080fe8de7d
ZhangLei <dya.ilaempire@gmail.com>
2022-02-11 10:04:10 +0800

    how-many-numbers-are-smaller-than-the-current-number

---
+++ src/main/java/com/raydya/HowManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -0,0 +1,23 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class HowManyNumbersAreSmallerThanTheCurrentNumber {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        final int[] ret = new int[nums.length];
+
+        final int[] t = nums.clone();
+        Arrays.sort(t);
+        final int[] h = new int[101];
+        Arrays.fill(h, -1);
+        for (int i = 0; i < t.length; i++) {
+            h[t[i]] = h[t[i]] == -1 ? i : h[t[i]];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            ret[i] = h[nums[i]];
+        }
+
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/HowManyNumbersAreSmallerThanTheCurrentNumberTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HowManyNumbersAreSmallerThanTheCurrentNumberTest {
+    @Test
+    public void test1() {
+        final HowManyNumbersAreSmallerThanTheCurrentNumber solution = new HowManyNumbersAreSmallerThanTheCurrentNumber();
+        final int[] output = solution.smallerNumbersThanCurrent(new int[]{8, 1, 2, 2, 3});
+
+        Assert.assertArrayEquals(new int[]{4, 0, 1, 1, 3}, output);
+    }
+
+    @Test
+    public void test2() {
+        final HowManyNumbersAreSmallerThanTheCurrentNumber solution = new HowManyNumbersAreSmallerThanTheCurrentNumber();
+        final int[] output = solution.smallerNumbersThanCurrent(new int[]{6, 5, 4, 8});
+
+        Assert.assertArrayEquals(new int[]{2, 1, 0, 3}, output);
+    }
+
+    @Test
+    public void test3() {
+        final HowManyNumbersAreSmallerThanTheCurrentNumber solution = new HowManyNumbersAreSmallerThanTheCurrentNumber();
+        final int[] output = solution.smallerNumbersThanCurrent(new int[]{7, 7, 7, 7});
+
+        Assert.assertArrayEquals(new int[]{0, 0, 0, 0}, output);
+    }
+}
