b00709123e82f7026cb5f9ca2da03c135a677710
ZhangLei <dya.ilaempire@gmail.com>
2022-02-14 14:23:33 +0800

    check-if-one-string-swap-can-make-strings-equal

---
+++ src/main/java/com/raydya/CheckIfOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,25 @@
+package com.raydya;
+
+import java.util.Objects;
+
+public class CheckIfOneStringSwapCanMakeStringsEqual {
+    public boolean areAlmostEqual(String s1, String s2) {
+        if (Objects.equals(s1, s2)) return true;
+        if (s1.length() != s2.length()) return false;
+        int falseCnt = 0;
+        final int[] h = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            final char c1 = s1.charAt(i);
+            final char c2 = s2.charAt(i);
+            h[c1 - 'a']++;
+            h[c2 - 'a']--;
+            if (c1 != c2) falseCnt++;
+        }
+
+        for (final int j : h) {
+            if (j != 0) return false;
+        }
+
+        return falseCnt == 2;
+    }
+}
---
+++ src/test/java/com/raydya/CheckIfOneStringSwapCanMakeStringsEqualTest.java
@@ -0,0 +1,46 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckIfOneStringSwapCanMakeStringsEqualTest {
+    @Test
+    public void test1() {
+        final CheckIfOneStringSwapCanMakeStringsEqual solution = new CheckIfOneStringSwapCanMakeStringsEqual();
+        final boolean output = solution.areAlmostEqual("bank", "kanb");
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final CheckIfOneStringSwapCanMakeStringsEqual solution = new CheckIfOneStringSwapCanMakeStringsEqual();
+        final boolean output = solution.areAlmostEqual("attack", "defend");
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test3() {
+        final CheckIfOneStringSwapCanMakeStringsEqual solution = new CheckIfOneStringSwapCanMakeStringsEqual();
+        final boolean output = solution.areAlmostEqual("kelb", "kelb");
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test4() {
+        final CheckIfOneStringSwapCanMakeStringsEqual solution = new CheckIfOneStringSwapCanMakeStringsEqual();
+        final boolean output = solution.areAlmostEqual("abcd", "dcba");
+
+        Assert.assertFalse(output);
+    }
+
+    @Test
+    public void test5() {
+        final CheckIfOneStringSwapCanMakeStringsEqual solution = new CheckIfOneStringSwapCanMakeStringsEqual();
+        final boolean output = solution.areAlmostEqual("caa", "aaz");
+
+        Assert.assertFalse(output);
+    }
+}
