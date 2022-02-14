53dcba0c2f0fc26b8b13e3eb9232af924e337082
ZhangLei <dya.ilaempire@gmail.com>
2022-02-14 13:55:23 +0800

    can-make-arithmetic-progression-from-sequence

---
+++ src/main/java/com/raydya/CanMakeArithmeticProgressionFromSequence.java
@@ -0,0 +1,16 @@
+package com.raydya;
+
+import java.util.Arrays;
+
+public class CanMakeArithmeticProgressionFromSequence {
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        Arrays.sort(arr);
+
+        int gap = arr[1] - arr[0];
+        for (int i = 2; i < arr.length; i++) {
+            if ((arr[i] - arr[i - 1]) != gap) return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/com/raydya/CanMakeArithmeticProgressionFromSequenceTest.java
@@ -0,0 +1,22 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CanMakeArithmeticProgressionFromSequenceTest {
+    @Test
+    public void test1() {
+        final CanMakeArithmeticProgressionFromSequence solution = new CanMakeArithmeticProgressionFromSequence();
+        final boolean output = solution.canMakeArithmeticProgression(new int[]{3, 5, 1});
+
+        Assert.assertTrue(output);
+    }
+
+    @Test
+    public void test2() {
+        final CanMakeArithmeticProgressionFromSequence solution = new CanMakeArithmeticProgressionFromSequence();
+        final boolean output = solution.canMakeArithmeticProgression(new int[]{1, 2, 4});
+
+        Assert.assertFalse(output);
+    }
+}
