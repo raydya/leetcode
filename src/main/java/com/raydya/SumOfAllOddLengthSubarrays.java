403b023a0cb1749ba317d5673bacd9ddac0fade3
ZhangLei <dya.ilaempire@gmail.com>
2022-02-16 14:28:25 +0800

    sum-of-all-odd-length-subarrays

---
+++ src/main/java/com/raydya/SumOfAllOddLengthSubarrays.java
@@ -0,0 +1,15 @@
+package com.raydya;
+
+public class SumOfAllOddLengthSubarrays {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int ret = 0;
+        for (int i = 1; i <= arr.length; i += 2) {
+            for (int j = 0; j <= arr.length - i; j++) {
+                for (int k = j; k < i + j; k++) {
+                    ret += arr[k];
+                }
+            }
+        }
+        return ret;
+    }
+}
---
+++ src/test/java/com/raydya/SumOfAllOddLengthSubarraysTest.java
@@ -0,0 +1,30 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SumOfAllOddLengthSubarraysTest {
+    @Test
+    public void test1() {
+        final SumOfAllOddLengthSubarrays solution = new SumOfAllOddLengthSubarrays();
+        final int output = solution.sumOddLengthSubarrays(new int[]{1, 4, 2, 5, 3});
+
+        Assert.assertEquals(58, output);
+    }
+
+    @Test
+    public void test2() {
+        final SumOfAllOddLengthSubarrays solution = new SumOfAllOddLengthSubarrays();
+        final int output = solution.sumOddLengthSubarrays(new int[]{1, 2});
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test3() {
+        final SumOfAllOddLengthSubarrays solution = new SumOfAllOddLengthSubarrays();
+        final int output = solution.sumOddLengthSubarrays(new int[]{10, 11, 12});
+
+        Assert.assertEquals(66, output);
+    }
+}
