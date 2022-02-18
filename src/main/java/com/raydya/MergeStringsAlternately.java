dfb527899ef66587407cbc20dce0a730c10a6456
ZhangLei <dya.ilaempire@gmail.com>
2022-02-18 17:43:57 +0800

    update-merge-strings-alternately

--- src/main/java/com/raydya/MergeStringsAlternately.java
+++ src/main/java/com/raydya/MergeStringsAlternately.java
@@ -6,17 +6,16 @@ public class MergeStringsAlternately {
         int p2 = 0;
 
         StringBuilder builder = new StringBuilder();
-        while (p1 < word1.length() && p2 < word2.length()) {
-            builder.append(word1.charAt(p1));
-            p1++;
-            builder.append(word2.charAt(p2));
-            p2++;
+        while (p1 < word1.length() || p2 < word2.length()) {
+            if (p1 < word1.length()) {
+                builder.append(word1.charAt(p1));
+                p1++;
+            }
+            if (p2 < word2.length()) {
+                builder.append(word2.charAt(p2));
+                p2++;
+            }
         }
-
-        final String m = word1.substring(p1);
-        builder.append(m);
-        final String n = word2.substring(p2);
-        builder.append(n);
 
         return builder.toString();
     }
