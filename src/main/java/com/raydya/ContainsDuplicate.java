317b36c02de72530335279363ee188fcb5e210aa
ZhangLei <dya.ilaempire@gmail.com>
2022-02-21 16:35:38 +0800

    using-google-java-code-type-formatting

--- src/main/java/com/raydya/ContainsDuplicate.java
+++ src/main/java/com/raydya/ContainsDuplicate.java
@@ -4,14 +4,15 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate {
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (final int num : nums) {
-            if (!set.add(num)) {
-                return true;
-            }
-        }
 
-        return false;
+  public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    for (final int num : nums) {
+      if (!set.add(num)) {
+        return true;
+      }
     }
+
+    return false;
+  }
 }
