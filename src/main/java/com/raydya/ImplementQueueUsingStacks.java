ba8835b7a27713e53cee2b8e41d7a11996d8ccca
ZhangLei <dya.ilaempire@gmail.com>
2022-02-21 11:21:31 +0800

    implement-queue-using-stacks

---
+++ src/main/java/com/raydya/ImplementQueueUsingStacks.java
@@ -0,0 +1,33 @@
+package com.raydya;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ImplementQueueUsingStacks {
+    private ImplementQueueUsingStacks() {
+    }
+
+    public static class MyQueue {
+        private final Deque<Integer> stack;
+
+        public MyQueue() {
+            this.stack = new LinkedList<>();
+        }
+
+        public void push(int x) {
+            stack.addLast(x);
+        }
+
+        public int pop() {
+            return stack.pop();
+        }
+
+        public int peek() {
+            return stack.peek();
+        }
+
+        public boolean empty() {
+            return stack.isEmpty();
+        }
+    }
+}
---
+++ src/test/java/com/raydya/ImplementQueueUsingStacksTest.java
@@ -0,0 +1,16 @@
+package com.raydya;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ImplementQueueUsingStacksTest {
+    @Test
+    public void test1() {
+        final ImplementQueueUsingStacks.MyQueue solution = new ImplementQueueUsingStacks.MyQueue();
+        solution.push(1);
+        solution.push(2);
+        Assert.assertEquals(1, solution.peek());
+        Assert.assertEquals(1, solution.pop());
+        Assert.assertFalse(solution.empty());
+    }
+}
