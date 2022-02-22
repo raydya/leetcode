50c45ccc949adc4efc2f2f50b98ca85a3adf62cd
ZhangLei <dya.ilaempire@gmail.com>
2022-02-22 10:39:09 +0800

    design-parking-system

---
+++ src/main/java/com/raydya/DesignParkingSystem.java
@@ -0,0 +1,43 @@
+package com.raydya;
+
+public class DesignParkingSystem {
+
+  public static class ParkingSystem {
+
+    private int bigSlot;
+    private int mediumSlot;
+    private int smallSlot;
+
+    public ParkingSystem(int big, int medium, int small) {
+      this.bigSlot = big;
+      this.mediumSlot = medium;
+      this.smallSlot = small;
+    }
+
+    public boolean addCar(int carType) {
+      switch (carType) {
+        case 1:
+          if (bigSlot <= 0) {
+            return false;
+          }
+          bigSlot--;
+          break;
+        case 2:
+          if (mediumSlot <= 0) {
+            return false;
+          }
+          mediumSlot--;
+          break;
+        case 3:
+          if (smallSlot <= 0) {
+            return false;
+          }
+          smallSlot--;
+          break;
+        default:
+          break;
+      }
+      return true;
+    }
+  }
+}
