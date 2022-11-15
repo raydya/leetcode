fb392a01943ad253f4703fa934225819a2231307
ZhangLei <dya.ilaempire@gmail.com>
2022-11-15 21:25:22 +0800

    more test

--- src/main/java/com/raydya/ethan/math/Sudoku.java
+++ src/main/java/com/raydya/ethan/math/Sudoku.java
@@ -5,10 +5,25 @@ import java.util.Arrays;
 public class Sudoku {
 
     private static final int N = 9;
+    private static final int S = 3;
 
-    public int[][] solve(final int[][] board) {
+    public int[][] solveSimple(final int[][] board) {
         final int[] candidates = new int[N];
         Arrays.fill(candidates, N);
+        final boolean[][][] ngm = new boolean[N][N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                final int c = board[i][j];
+                Arrays.fill(ngm[i][j], true);
+                if (c > 0) {
+                    for (int n = 0; n < N; n++) {
+                        ngm[i][n][c - 1] = false;
+                        ngm[n][j][c - 1] = false;
+                    }
+                }
+            }
+        }
 
         for (final int[] r : board) {
             for (final int c : r) {
@@ -21,17 +36,15 @@ public class Sudoku {
         int i = 0;
         int j = 0;
 
-        final boolean[] p = new boolean[N];
-        Arrays.fill(p, true);
-
         while (!isFinished(candidates)) {
 
             final int x = board[i][j];
             if (x == 0) {
                 // row
-                for (final int r : board[i]) {
+                for (int k = 0; k < N; k++) {
+                    final int r = board[i][k];
                     if (r > 0) {
-                        p[r - 1] = false;
+                        ngm[i][j][r - 1] = false;
                     }
                 }
 
@@ -39,7 +52,7 @@ public class Sudoku {
                 for (int k = 0; k < N; k++) {
                     final int v = board[k][j];
                     if (v > 0) {
-                        p[v - 1] = false;
+                        ngm[i][j][v - 1] = false;
                     }
                 }
 
@@ -50,12 +63,12 @@ public class Sudoku {
                     for (int n = ny; n < ny + 3; n++) {
                         final int v = board[m][n];
                         if (v > 0) {
-                            p[v - 1] = false;
+                            ngm[i][j][v - 1] = false;
                         }
                     }
                 }
 
-                final int bingo = bingo(p);
+                final int bingo = bingo(i, j, ngm);
                 if (bingo > 0) {
                     board[i][j] = bingo;
                     candidates[bingo - 1]--;
@@ -64,25 +77,26 @@ public class Sudoku {
 
             i = (i + ((j + 1) / N)) % N;
             j = (j + 1) % N;
-            Arrays.fill(p, true);
         }
 
         return board;
     }
 
-    private int bingo(final boolean[] p) {
-        int x = 0;
-        for (int i = 0; i < p.length; i++) {
+    private int bingo(final int x, final int y, final boolean[][][] ngm) {
+        final boolean[] p = ngm[x][y];
+        int t = 0;
+        // 单格仅有一个可能
+        for (int i = 0; i < N; i++) {
             if (p[i]) {
-                if (x == 0) {
-                    x = i + 1;
+                if (t == 0) {
+                    t = i + 1;
                 } else {
-                    return -1;
+                    t = 0;
+                    break;
                 }
             }
         }
-
-        return x;
+        return t;
     }
 
     private boolean isFinished(final int[] candidates) {
@@ -92,6 +106,44 @@ public class Sudoku {
             }
         }
         return true;
+    }
+
+
+    public int[][] solveHard(final int[][] board) {
+        final int[][][] boxes = toNineBox(board);
+
+        return board;
+    }
+
+    private int[][][] toNineBox(final int[][] board) {
+        final int[][][] box = new int[N][S][S];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                final int[] xy = xy(i, j);
+                final int b = xy[0];
+                final int m = xy[1];
+                final int n = xy[2];
+                box[b][m][n] = board[i][j];
+            }
+        }
+
+        return box;
+    }
+
+    public int[] xy(final int v, final int h) {
+        int b = (v / 3 * 3) + h / 3;
+        int bv = v % 3;
+        int bh = h % 3;
+
+        return new int[]{b, bv, bh};
+    }
+
+    public int[] rxy(int[] xy) {
+        final int b = xy[0];
+        final int mo = b / 3 * 3;
+        final int no = b % 3 * 3;
+        return new int[]{mo + xy[1], no + xy[2]};
     }
 
 }
--- src/test/java/com/raydya/ethan/math/SudokuTest.java
+++ src/test/java/com/raydya/ethan/math/SudokuTest.java
@@ -19,7 +19,7 @@ public class SudokuTest {
             {0, 1, 5, 6, 9, 2, 0, 7, 4}
         };
 
-        final int[][] output = new Sudoku().solve(board);
+        final int[][] output = new Sudoku().solveSimple(board);
 
         final int[][] answer = {
             {5, 2, 3, 9, 1, 4, 6, 8, 7},
@@ -35,5 +35,104 @@ public class SudokuTest {
 
         Assert.assertArrayEquals(answer, output);
     }
+
+    @Test
+    public void test2() {
+        final int[][] board = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 2, 8, 0},
+            {3, 7, 6, 4, 0, 0, 0, 0, 0},
+            {7, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 2, 0, 0, 0, 0, 0, 0, 0},
+            {4, 0, 0, 3, 0, 0, 0, 0, 6},
+            {0, 1, 0, 0, 2, 8, 0, 0, 0},
+            {0, 0, 0, 0, 0, 5, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 3}
+        };
+
+        final int[][] output = new Sudoku().solveHard(board);
+
+        final int[][] answer = {
+            {8, 4, 2, 5, 1, 9, 3, 6, 7},
+            {1, 5, 9, 6, 7, 3, 2, 8, 4},
+            {3, 7, 6, 4, 8, 2, 9, 5, 1},
+            {7, 3, 5, 2, 6, 1, 4, 9, 8},
+            {6, 2, 1, 8, 9, 4, 7, 3, 5},
+            {4, 9, 8, 3, 5, 7, 1, 2, 6},
+            {5, 1, 3, 7, 2, 8, 6, 4, 9},
+            {9, 6, 4, 1, 3, 5, 8, 7, 2},
+            {2, 8, 7, 9, 4, 6, 5, 1, 3}
+        };
+
+        Assert.assertArrayEquals(answer, output);
+    }
+
+    @Test
+    public void test3() {
+        final int[] xy = new Sudoku().xy(4, 8);
+
+        Assert.assertEquals(5, xy[0]);
+        Assert.assertEquals(1, xy[1]);
+        Assert.assertEquals(2, xy[2]);
+    }
+
+    @Test
+    public void test4() {
+        final int[] xy = new Sudoku().xy(8, 8);
+
+        Assert.assertEquals(8, xy[0]);
+        Assert.assertEquals(2, xy[1]);
+        Assert.assertEquals(2, xy[2]);
+    }
+
+    @Test
+    public void test5() {
+        final int[] input = {8, 2, 2};
+        final int[] output = new Sudoku().rxy(input);
+
+        Assert.assertEquals(8, output[0]);
+        Assert.assertEquals(8, output[1]);
+    }
+
+    @Test
+    public void test6() {
+        final int[] input = {5, 1, 2};
+        final int[] output = new Sudoku().rxy(input);
+
+        Assert.assertEquals(4, output[0]);
+        Assert.assertEquals(8, output[1]);
+    }
+
+    @Test
+    public void test7() {
+        final int[][] board = {
+            {6, 8, 0, 0, 0, 0, 2, 3, 0},
+            {9, 0, 0, 0, 0, 4, 0, 0, 5},
+            {2, 0, 7, 0, 0, 8, 0, 0, 0},
+            {0, 6, 0, 0, 0, 0, 0, 5, 8},
+            {0, 0, 2, 5, 0, 0, 1, 4, 0},
+            {0, 9, 0, 0, 0, 0, 0, 2, 0},
+            {1, 0, 4, 8, 7, 0, 9, 0, 0},
+            {0, 0, 0, 0, 2, 0, 4, 0, 0},
+            {0, 2, 6, 0, 0, 3, 5, 0, 7}
+        };
+
+        final int[][] output = new Sudoku().solveSimple(board);
+
+        final int[][] answer = {
+            {6, 8, 5, 7, 1, 9, 2, 3, 4},
+            {9, 1, 3, 2, 6, 4, 8, 7, 5},
+            {2, 4, 7, 3, 5, 8, 6, 9, 1},
+            {4, 6, 1, 9, 3, 2, 7, 5, 8},
+            {3, 7, 2, 5, 8, 6, 1, 4, 9},
+            {5, 9, 8, 1, 4, 7, 3, 2, 6},
+            {1, 3, 4, 8, 7, 5, 9, 6, 2},
+            {7, 5, 9, 6, 2, 1, 4, 8, 3},
+            {8, 2, 6, 4, 9, 3, 5, 1, 7}
+        };
+
+        Assert.assertArrayEquals(answer, output);
+    }
+
 
 }
