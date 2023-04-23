7ac0fb407fefed393d0e44ed552a6e421a7cc33d
ZhangLei <dya.ilaempire@gmail.com>
2023-04-23 11:34:31 +0800

    number-of-different-integers-in-a-string

--- src/main/java/com/raydya/NumberOfDifferentIntegersInAString.java
+++ src/main/java/com/raydya/NumberOfDifferentIntegersInAString.java
@@ -6,24 +6,57 @@ import java.util.Set;
 public class NumberOfDifferentIntegersInAString {
 
     public int numDifferentIntegers(String word) {
-        char p = word.charAt(0);
         StringBuilder b = new StringBuilder();
-        final Set<Integer> s = new HashSet<>();
+        final Set<String> s = new HashSet<>();
+        final char f = word.charAt(0);
+        if (Character.isDigit(f)) {
+            b.append(f);
+        }
         for (int i = 1; i < word.length(); i++) {
             final char c = word.charAt(i);
-            if (Character.isDigit(c) && Character.isLetter(p)) {
+            if (Character.isDigit(c)) {
                 b.append(c);
-            } else if (Character.isLetter(c) && Character.isDigit(p)) {
-                final int k = Integer.parseInt(b.toString());
-                s.add(k);
-                b = new StringBuilder();
             } else {
-                b.append(c);
+                if (b.length() == 0) {
+                    continue;
+                }
+                parse(b);
+                if (b.length() == 0) {
+                    s.add("0");
+                } else {
+                    s.add(b.toString());
+                }
+                b = new StringBuilder();
             }
-            p = c;
+        }
+
+        if (b.length() != 0) {
+            parse(b);
+
+            if (b.length() == 0) {
+                s.add("0");
+            } else {
+                s.add(b.toString());
+            }
         }
 
         return s.size();
+    }
+
+    private void parse(StringBuilder b) {
+        int d = -1;
+        for (int j = 0; j < b.length(); j++) {
+            final char k = b.charAt(j);
+            if (k == '0') {
+                d++;
+            } else {
+                break;
+            }
+        }
+
+        if (d >= 0) {
+            b.delete(0, d + 1);
+        }
     }
 
 }
--- src/test/java/com/raydya/NumberOfDifferentIntegersInAStringTest.java
+++ src/test/java/com/raydya/NumberOfDifferentIntegersInAStringTest.java
@@ -28,4 +28,37 @@ public class NumberOfDifferentIntegersInAStringTest {
 
         Assert.assertEquals(1, output);
     }
+
+    @Test
+    public void test4() {
+        final NumberOfDifferentIntegersInAString solution = new NumberOfDifferentIntegersInAString();
+        final int output = solution.numDifferentIntegers("4");
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test5() {
+        final NumberOfDifferentIntegersInAString solution = new NumberOfDifferentIntegersInAString();
+        final int output = solution.numDifferentIntegers("167278959591294");
+
+        Assert.assertEquals(1, output);
+    }
+
+    @Test
+    public void test6() {
+        final NumberOfDifferentIntegersInAString solution = new NumberOfDifferentIntegersInAString();
+        final int output = solution.numDifferentIntegers(
+            "2393706880236110407059624696967828762752651982730115221690437821508229419410771541532394006597463715513741725852432559057224478815116557380260390432211227579663571046845842281704281749571110076974264971989893607137140456254346955633455446057823738757323149856858154529105301197388177242583658641529908583934918768953462557716z97438020429952944646288084173334701047574188936201324845149110176716130267041674438237608038734431519439828191344238609567530399189316846359766256507371240530620697102864238792350289978450509162697068948604722646739174590530336510475061521094503850598453536706982695212493902968251702853203929616930291257062173c79487281900662343830648295410");
+
+        Assert.assertEquals(3, output);
+    }
+
+    @Test
+    public void test7() {
+        final NumberOfDifferentIntegersInAString solution = new NumberOfDifferentIntegersInAString();
+        final int output = solution.numDifferentIntegers("4w31am0ets6sl5go5ufytjtjpb7b0sxqbee2blg9ss");
+
+        Assert.assertEquals(8, output);
+    }
 }
