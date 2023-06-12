76ba70205f0b6a8b00e3a7cf3804113f8155e257
ZhangLei <dya.ilaempire@gmail.com>
2023-06-12 16:40:08 +0800

    remove-one-element-to-make-the-array-strictly-increasing updated-v2

--- src/main/java/com/raydya/RemoveOneElementToMakeTheArrayStrictlyIncreasing.java
+++ src/main/java/com/raydya/RemoveOneElementToMakeTheArrayStrictlyIncreasing.java
@@ -3,32 +3,28 @@ package com.raydya;
 public class RemoveOneElementToMakeTheArrayStrictlyIncreasing {
 
     public boolean canBeIncreasing(int[] nums) {
-        final int[][] h = new int[nums.length][nums.length - 1];
-        for (int i = 0; i < nums.length; i++) {
+        final int l = nums.length;
+        final int[][] h = new int[l][l - 1];
+        for (int i = 0; i < l; i++) {
             int k = 0;
+            boolean f = true;
 
-            for (int j = 0; j < nums.length; j++) {
+            for (int j = 0; j < l; j++) {
                 if (i == j) {
                     continue;
                 }
                 h[i][k] = nums[j];
+
+                if (k - 1 >= 0 && h[i][k - 1] >= h[i][k]) {
+                    f = false;
+                }
                 k++;
             }
-        }
 
-        for (final int[] k : h) {
-            boolean f = true;
-            for (int j = 1; j < k.length; j++) {
-                if (k[j - 1] < k[j]) {
-                    continue;
-                }
-                f = false;
-            }
             if (f) {
                 return true;
             }
         }
-
         return false;
     }
 }
