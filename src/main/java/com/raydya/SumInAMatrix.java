71ed311189c965eb26599658b21c296ae45425bf
ZhangLei <dya.ilaempire@gmail.com>
2023-07-04 11:45:26 +0800

    sum-in-a-matrix

--- src/main/java/com/raydya/SumInAMatrix.java
+++ src/main/java/com/raydya/SumInAMatrix.java
@@ -1,22 +1,22 @@
 package com.raydya;
 
+import java.util.Arrays;
+
 public class SumInAMatrix {
 
     public int matrixSum(int[][] nums) {
         int r = 0;
         int k = 0;
         int l = nums[0].length;
+        for (final int[] row : nums) {
+            Arrays.sort(row);
+        }
+
         while (k < l) {
             int t = 0;
             for (final int[] row : nums) {
-                int m = 0;
-                int z = 0;
-                for (int j = 0; j < row.length; j++) {
-                    if (row[j] > m) {
-                        m = row[j];
-                        z = j;
-                    }
-                }
+                int m = row[l - 1 - k];
+                int z = l - 1 - k;
                 row[z] = -1;
                 t = Math.max(t, m);
             }
