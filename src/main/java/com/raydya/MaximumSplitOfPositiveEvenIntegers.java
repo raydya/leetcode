634ce05cc15b57672fa585db7293e6559f064bbd
ZhangLei <dya.ilaempire@gmail.com>
2023-07-06 10:47:57 +0800

    maximum-split-of-positive-even-integers-optimized

--- src/main/java/com/raydya/MaximumSplitOfPositiveEvenIntegers.java
+++ src/main/java/com/raydya/MaximumSplitOfPositiveEvenIntegers.java
@@ -11,21 +11,13 @@ public class MaximumSplitOfPositiveEvenIntegers {
             return new ArrayList<>();
         }
 
-        long k = 0;
-        int i = 1;
         final List<Long> h = new ArrayList<>();
-        while (k < finalSum) {
-            final long n = i * 2L;
-            if (k + n > finalSum) {
-                break;
-            }
-            h.add(n);
-            k += n;
-            i++;
+        for (long i = 2L; i <= finalSum; i += 2) {
+            h.add(i);
+            finalSum -= i;
         }
 
-        final long d = finalSum - k;
-        h.set(h.size() - 1, h.get(h.size() - 1) + d);
+        h.set(h.size() - 1, h.get(h.size() - 1) + finalSum);
 
         return h;
     }
