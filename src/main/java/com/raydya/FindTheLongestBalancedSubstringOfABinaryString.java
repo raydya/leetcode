d2533732bdc169a986f1c0b0d00b6fe55e6d7587
dya.zhang <dya.ilaempire@gmail.com>
2023-11-08 19:24:42 +0800

    optimize-find-the-longest-balanced-substring-of-a-binary-string

--- src/main/java/com/raydya/FindTheLongestBalancedSubstringOfABinaryString.java
+++ src/main/java/com/raydya/FindTheLongestBalancedSubstringOfABinaryString.java
@@ -3,24 +3,21 @@ package com.raydya;
 public class FindTheLongestBalancedSubstringOfABinaryString {
 
     public int findTheLongestBalancedSubstring(String s) {
-        int l = s.length() / 2;
-        while (l > 0) {
-            final StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < l; i++) {
-                builder.append('0');
+        int res = 0;
+        int n = s.length();
+        int[] count = new int[2];
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                count[1]++;
+                res = Math.max(res, 2 * Math.min(count[0], count[1]));
+            } else if (i == 0 || s.charAt(i - 1) == '1') {
+                count[0] = 1;
+                count[1] = 0;
+            } else {
+                count[0]++;
             }
-            for (int i = 0; i < l; i++) {
-                builder.append('1');
-            }
-            final String str = builder.toString();
-            final int i = s.indexOf(str);
-            if (i >= 0) {
-                return str.length();
-            }
-            l = l - 1;
         }
-
-        return 0;
+        return res;
     }
 
 }
