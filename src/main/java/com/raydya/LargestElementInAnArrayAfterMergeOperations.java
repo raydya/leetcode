0a1fef5a71c5b34adb4238e3f775af76c060c050
dya.zhang <dya.ilaempire@gmail.com>
2024-03-14 13:42:12 +0800

    optimize:largest-element-in-an-array-after-merge-operations

--- src/main/java/com/raydya/LargestElementInAnArrayAfterMergeOperations.java
+++ src/main/java/com/raydya/LargestElementInAnArrayAfterMergeOperations.java
@@ -1,34 +1,13 @@
 package com.raydya;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LargestElementInAnArrayAfterMergeOperations {
 
     public long maxArrayValue(int[] nums) {
-        List<Long> list = new ArrayList<>();
-        for (int num : nums) {
-            list.add((long) num);
+        long sum = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            sum = nums[i] <= sum ? nums[i] + sum : nums[i];
         }
-
-        boolean merged;
-        do {
-            merged = false;
-            for (int i = list.size() - 2; i >= 0; --i) {
-                if (list.get(i) <= list.get(i + 1)) {
-                    long sum = list.get(i) + list.get(i + 1);
-                    list.set(i + 1, sum);
-                    list.remove(i);
-                    merged = true;
-                }
-            }
-        } while (merged);
-
-        long max = 0;
-        for (long num : list) {
-            max = Math.max(max, num);
-        }
-        return max;
+        return sum;
     }
 
 }
